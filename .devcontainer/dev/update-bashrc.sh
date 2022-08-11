2017589ad767b0a64e31fec98628c2736f2e377c
Patrick Creighton <pcreighton429@gmail.com>
2022-08-10 22:48:49 -0700

    Remove redundant display bash configuration

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -16,7 +16,3 @@ echo "# setup ROS 2 environment" >> ~/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "source /workspaces/sailbot_ros2_workspace/install/setup.bash" >> ~/.bashrc
 echo "" >> ~/.bashrc
-
-echo "# Set up X11 forwarding for GUI apps" >> ~/.bashrc
-echo "export DISPLAY=:0" >> ~/.bashrc
-echo "" >> ~/.bashrc
