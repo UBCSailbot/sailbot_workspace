cd2edd3ca43e58a577363146f7ab2a783cb1dcd3
Patrick Creighton <pcreighton429@gmail.com>
2022-08-11 08:52:09 +0000

    Add curly braces around env variables

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -14,7 +14,7 @@ echo "source /workspaces/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "# setup ROS 2 environment" >> ~/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
-echo "source /opt/ros/$ROS2_DISTRO/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/${ROS2_DISTRO}/setup.bash" >> ~/.bashrc
 echo "if [ -f /workspaces/vscode_ros2_workspace/install/local_setup.bash ]" >> ~/.bashrc
 echo "then" >> ~/.bashrc
 echo "    source /workspaces/vscode_ros2_workspace/install/local_setup.bash" >> ~/.bashrc
@@ -22,4 +22,4 @@ echo "else" >> ~/.bashrc
 echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then source ~/.bashrc'" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
 echo "# place ROS 2 Python site packages first in PYTHONPATH" >> ~/.bashrc
-echo "export PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:$PYTHONPATH" >> ~/.bashrc
+echo "export PYTHONPATH=/opt/ros/${ROS2_DISTRO}/lib/python3.6/site-packages:${PYTHONPATH}" >> ~/.bashrc
