59b03e9d64dba9f3a4b5d0056c2ad1f5027afc0e
Patrick Creighton <pcreighton429@gmail.com>
2022-08-11 11:00:27 +0000

    Clean up dev image

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -3,23 +3,23 @@ echo "# setup ROS" >> ~/.bashrc
 
 echo "" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
-echo "export _colcon_cd_root=/workspaces/vscode_ros2_workspace" >> ~/.bashrc
+echo "export _colcon_cd_root=$ROS2_WORKSPACE" >> ~/.bashrc
 
 echo "" >> ~/.bashrc
 echo "# setup ROS 1 environment" >> ~/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
-echo "export ROS_WORKSPACE=/workspaces/catkin_ws" >> ~/.bashrc
+echo "export ROS_WORKSPACE=$ROS1_WORKSPACE" >> ~/.bashrc
 echo "source /opt/ros/$ROS1_DISTRO/setup.bash" >> ~/.bashrc
-echo "alias srcraye='unset ROS_DISTRO; source /workspaces/catkin_ws/devel/setup.bash'" >> ~/.bashrc
+echo "alias srcraye='unset ROS_DISTRO; source $ROS1_WORKSPACE/devel/setup.bash'" >> ~/.bashrc
 
 echo "" >> ~/.bashrc
 echo "# setup ROS 2 environment" >> ~/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
-echo "source /opt/ros/${ROS2_DISTRO}/setup.bash" >> ~/.bashrc
-echo "if [ -f /workspaces/vscode_ros2_workspace/install/local_setup.bash ]" >> ~/.bashrc
+echo "source /opt/ros/$ROS2_DISTRO/setup.bash" >> ~/.bashrc
+echo "if [ -f $ROS2_WORKSPACE/install/local_setup.bash ]" >> ~/.bashrc
 echo "then" >> ~/.bashrc
-echo "    source /workspaces/vscode_ros2_workspace/install/local_setup.bash" >> ~/.bashrc
+echo "    source $ROS2_WORKSPACE/install/local_setup.bash" >> ~/.bashrc
 echo "else" >> ~/.bashrc
 echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then run srcnew'" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
-echo "alias srcnew='unset ROS_DISTRO; source /workspaces/vscode_ros2_workspace/install/local_setup.bash'" >> ~/.bashrc
+echo "alias srcnew='unset ROS_DISTRO; source $ROS2_WORKSPACE/install/local_setup.bash'" >> ~/.bashrc
