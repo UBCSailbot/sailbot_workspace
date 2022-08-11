e6815d0531719756ce70692e0f48df1ac9f45029
Patrick Creighton <pcreighton429@gmail.com>
2022-08-11 06:56:30 +0000

    Fix ROS sourcing

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -1,18 +1,23 @@
 echo "" >> ~/.bashrc
 echo "# Setup ROS" >> ~/.bashrc
-echo "" >> ~/.bashrc
 
+echo "" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
-echo "export _colcon_cd_root=/workspaces/sailbot_ros2_workspace" >> ~/.bashrc
-echo "" >> ~/.bashrc
+echo "export _colcon_cd_root=/workspaces/vscode_ros2_workspace" >> ~/.bashrc
 
+echo "" >> ~/.bashrc
 echo "# setup ROS 1 environment" >> ~/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "export ROS_WORKSPACE=/workspaces/catkin_ws" >> ~/.bashrc
 echo "source /workspaces/catkin_ws/devel/setup.bash" >> ~/.bashrc
-echo "" >> ~/.bashrc
 
+echo "" >> ~/.bashrc
 echo "# setup ROS 2 environment" >> ~/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
-echo "source /workspaces/sailbot_ros2_workspace/install/setup.bash" >> ~/.bashrc
-echo "" >> ~/.bashrc
+echo "source /opt/ros/${ROS2_DISTRO}/setup.bash" >> ~/.bashrc
+echo "if [ -f /workspaces/vscode_ros2_workspace/install/local_setup.bash ]" >> ~/.bashrc
+echo "then" >> ~/.bashrc
+echo "    source /workspaces/vscode_ros2_workspace/install/local_setup.bash" >> ~/.bashrc
+echo "else" >> ~/.bashrc
+echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then source ~/.bashrc'" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
