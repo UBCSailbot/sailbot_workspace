f95d62c8aa679b39002775ddeb2f1c94fe09d75a
Patrick Creighton <pcreighton429@gmail.com>
2022-08-11 11:53:47 +0000

    Fix ros 2 sourcing

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -12,10 +12,10 @@ echo "source /opt/ros/$ROS2_DISTRO/setup.bash" >> ~/.bashrc
 echo "if [ -f $ROS2_WORKSPACE/install/local_setup.bash ]" >> ~/.bashrc
 echo "then" >> ~/.bashrc
 echo "    source $ROS2_WORKSPACE/install/local_setup.bash" >> ~/.bashrc
-echo "    export PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:$PYTHONPATH" >> ~/.bashrc
+echo "    export PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH" >> ~/.bashrc
 echo "else" >> ~/.bashrc
 echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then run srcnew'" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
-echo "alias srcnew='unset ROS_DISTRO; source /workspaces/vscode_ros2_workspace/install/local_setup.bash; PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:$PYTHONPATH'" >> ~/.bashrc
+echo "alias srcnew='unset ROS_DISTRO; source /workspaces/vscode_ros2_workspace/install/local_setup.bash; PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH'" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 echo "export _colcon_cd_root=$ROS2_WORKSPACE" >> ~/.bashrc
