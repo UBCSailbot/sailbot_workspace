efbb6b175d430c8d568c716d91579f0d55d5d990
Patrick Creighton <pcreighton429@gmail.com>
2022-08-11 11:50:40 +0000

    Fix ROS 2 sourcing

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -1,11 +1,4 @@
 echo "" >> ~/.bashrc
-echo "# setup ROS" >> ~/.bashrc
-
-echo "" >> ~/.bashrc
-echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
-echo "export _colcon_cd_root=$ROS2_WORKSPACE" >> ~/.bashrc
-
-echo "" >> ~/.bashrc
 echo "# setup ROS 1 environment" >> ~/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "export ROS_WORKSPACE=$ROS1_WORKSPACE" >> ~/.bashrc
@@ -19,7 +12,10 @@ echo "source /opt/ros/$ROS2_DISTRO/setup.bash" >> ~/.bashrc
 echo "if [ -f $ROS2_WORKSPACE/install/local_setup.bash ]" >> ~/.bashrc
 echo "then" >> ~/.bashrc
 echo "    source $ROS2_WORKSPACE/install/local_setup.bash" >> ~/.bashrc
+echo "    export PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:$PYTHONPATH" >> ~/.bashrc
 echo "else" >> ~/.bashrc
 echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then run srcnew'" >> ~/.bashrc
 echo "fi" >> ~/.bashrc
-echo "alias srcnew='unset ROS_DISTRO; source $ROS2_WORKSPACE/install/local_setup.bash'" >> ~/.bashrc
+echo "alias srcnew='unset ROS_DISTRO; source /workspaces/vscode_ros2_workspace/install/local_setup.bash; PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:$PYTHONPATH'" >> ~/.bashrc
+echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
+echo "export _colcon_cd_root=$ROS2_WORKSPACE" >> ~/.bashrc
