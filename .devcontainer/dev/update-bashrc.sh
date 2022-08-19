5796ada312f95ecd404bedce9e044f1f95f12265
Patrick Creighton <pcreighton429@gmail.com>
2022-08-19 21:42:50 +0000

    Remove debugging commands and add back dev image configuration

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -1,21 +1,21 @@
-echo "" >> ~/.bashrc
-echo "# setup ROS 1 environment" >> ~/.bashrc
-echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
-echo "export ROS_WORKSPACE=$ROS1_WORKSPACE" >> ~/.bashrc
-echo "source /opt/ros/$ROS1_DISTRO/setup.bash" >> ~/.bashrc
-echo "alias srcraye='unset ROS_DISTRO; source $ROS1_WORKSPACE/devel/setup.bash'" >> ~/.bashrc
+echo "" >> /home/$USERNAME/.bashrc
+echo "# setup ROS 1 environment" >> /home/$USERNAME/.bashrc
+echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> /home/$USERNAME/.bashrc
+echo "export ROS_WORKSPAcE=$ROS1_WORKSPACE" >> /home/$USERNAME/.bashrc
+echo "source /opt/ros/$ROS1_DISTRO/setup.bash" >> /home/$USERNAME/.bashrc
+echo "alias srcraye='unset ROS_DISTRO; source $ROS1_WORKSPACE/devel/setup.bash'" >> /home/$USERNAME/.bashrc
 
-echo "" >> ~/.bashrc
-echo "# setup ROS 2 environment" >> ~/.bashrc
-echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
-echo "source /opt/ros/$ROS2_DISTRO/setup.bash" >> ~/.bashrc
-echo "if [ -f $ROS2_WORKSPACE/install/local_setup.bash ]" >> ~/.bashrc
-echo "then" >> ~/.bashrc
-echo "    source $ROS2_WORKSPACE/install/local_setup.bash" >> ~/.bashrc
-echo "    export PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH" >> ~/.bashrc
-echo "else" >> ~/.bashrc
-echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then run srcnew'" >> ~/.bashrc
-echo "fi" >> ~/.bashrc
-echo "alias srcnew='unset ROS_DISTRO; source /workspaces/vscode_ros2_workspace/install/local_setup.bash; PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH'" >> ~/.bashrc
-echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
-echo "export _colcon_cd_root=$ROS2_WORKSPACE" >> ~/.bashrc
+echo "" >> /home/$USERNAME/.bashrc
+echo "# setup ROS 2 environment" >> /home/$USERNAME/.bashrc
+echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> /home/$USERNAME/.bashrc
+echo "source /opt/ros/$ROS2_DISTRO/setup.bash" >> /home/$USERNAME/.bashrc
+echo "if [ -f $ROS2_WORKSPACE/install/local_setup.bash ]" >> /home/$USERNAME/.bashrc
+echo "then" >> /home/$USERNAME/.bashrc
+echo "    source $ROS2_WORKSPACE/install/local_setup.bash" >> /home/$USERNAME/.bashrc
+echo "    export PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH" >> /home/$USERNAME/.bashrc
+echo "else" >> /home/$USERNAME/.bashrc
+echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then run srcnew'" >> /home/$USERNAME/.bashrc
+echo "fi" >> /home/$USERNAME/.bashrc
+echo "alias srcnew='unset ROS_DISTRO; source /workspaces/vscode_ros2_workspace/install/local_setup.bash; PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH'" >> /home/$USERNAME/.bashrc
+echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> /home/$USERNAME/.bashrc
+echo "export _colcon_cd_root=$ROS2_WORKSPACE" >> /home/$USERNAME/.bashrc
--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-echo $USER
-echo $HOME
-id -u $USER
-id -g $USER
-git --version
-GIT_CURL_VERBOSE=1 GIT_TRACE=1 git ls-remote https://github.com/UBCSailbot/py_pubsub main
-ls -lah src
-vcs --version
 vcs import < src/ros2.repos src
 sudo apt-get update
 rosdep update
