48e908e6cc995e8123c4dbb86a38e3de32813f2c
Patrick Creighton <pcreighton429@gmail.com>
2022-08-19 22:13:50 +0000

    Attempt 3 to fix rosdep install

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ set -e
 
 vcs import < src/ros2.repos src
 sudo apt-get update
+source /opt/ros/${ROS2_DISTRO}/setup.bash
 rosdep update
-rosdep install --from-paths src --ignore-src -y --os=ubuntu:bionic --rosdistro=${ROS2_DISTRO}
+rosdep install --from-paths src --ignore-src -y
