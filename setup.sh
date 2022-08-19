dc45dd10277fd46aea5ceb0a9a6e97320da56713
Patrick Creighton <pcreighton429@gmail.com>
2022-08-19 22:22:43 +0000

    Move ROS sourcing in script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@ set -e
 
 vcs import < src/ros2.repos src
 sudo apt-get update
-source /opt/ros/${ROS2_DISTRO}/setup.bash
 rosdep update
 rosdep install --from-paths src --ignore-src -y
--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-source /opt/ros/${ROS2_DISTRO}/setup.bash
 if [ -f install/local_setup.bash ]; then source install/local_setup.bash; fi
 colcon test --merge-install
 colcon test-result
