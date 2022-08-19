03b69b3a1f2fd42dd1750418bd244386764f21ee
Patrick Creighton <pcreighton429@gmail.com>
2022-08-19 20:27:22 +0000

    Add --debug to vcs import

--- setup.sh
+++ setup.sh
@@ -5,9 +5,10 @@ echo $USER
 echo $HOME
 id -u $USER
 id -g $USER
-ls -lah src
 git clone https://github.com/UBCSailbot/py_pubsub src/py_pubsub
-vcs import < src/ros2.repos src
+ls -lah src
+vcs --version
+vcs import < src/ros2.repos src --debug
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
