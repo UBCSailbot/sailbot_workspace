dfbfc61037863be485cbdfbef93590b50857a89e
Patrick Creighton <pcreighton429@gmail.com>
2022-08-19 20:48:12 +0000

    Add debug to vcs failing command

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo $USER
 echo $HOME
 id -u $USER
 id -g $USER
-git ls-remote https://github.com/UBCSailbot/py_pubsub main
+GIT_CURL_VERBOSE=1 GIT_TRACE=1 git ls-remote https://github.com/UBCSailbot/py_pubsub main
 ls -lah src
 vcs --version
 vcs import < src/ros2.repos src
