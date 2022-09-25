fac226bbda851b28a11039ccc09b1cb58770162c
Patrick Creighton <pcreighton429@gmail.com>
2022-09-25 21:34:48 +0000

    Update ros sourcing alias description

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -6,7 +6,7 @@ echo "if [ -f $HOME/.functions.bash ]; then . $HOME/.functions.bash; fi" >> $HOM
 echo "" >> $HOME/.bashrc
 echo "# setup ROS 1 environment" >> $HOME/.bashrc
 echo "export ROS_WORKSPACE=$ROS1_WORKSPACE" >> $HOME/.bashrc
-echo "alias srcraye='unset ROS_DISTRO; source $ROS1_WORKSPACE/devel/setup.bash; echo \"sourcing the underlay and overlay for $ROS1_WORKSPACE\"'" >> $HOME/.bashrc
+echo "alias srcraye='unset ROS_DISTRO; source $ROS1_WORKSPACE/devel/setup.bash; echo \"sourcing the underlay and overlay in $ROS1_WORKSPACE\"'" >> $HOME/.bashrc
 
 echo "" >> $HOME/.bashrc
 echo "# setup ROS 2 environment" >> $HOME/.bashrc
@@ -19,6 +19,6 @@ echo "    export PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$
 echo "else" >> $HOME/.bashrc
 echo "    echo 'ROS 2 workspace local_setup.bash not found; run the Build task then run srcnew'" >> $HOME/.bashrc
 echo "fi" >> $HOME/.bashrc
-echo "alias srcnew='unset ROS_DISTRO; source $ROS2_WORKSPACE/install/local_setup.bash; PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH; echo \"sourcing the overlay for $ROS2_WORKSPACE\"'" >> $HOME/.bashrc
+echo "alias srcnew='unset ROS_DISTRO; source $ROS2_WORKSPACE/install/local_setup.bash; PYTHONPATH=/opt/ros/$ROS2_DISTRO/lib/python3.6/site-packages:\$PYTHONPATH; echo \"sourcing the overlay in $ROS2_WORKSPACE\"'" >> $HOME/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> $HOME/.bashrc
 echo "export _colcon_cd_root=$ROS2_WORKSPACE" >> $HOME/.bashrc
