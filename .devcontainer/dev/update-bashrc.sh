ab7b83f043b2a75651ba8bf064ed6e8617381b80
Patrick Creighton <pcreighton429@gmail.com>
2022-10-22 17:48:57 +0000

    Fix bashrc

--- .devcontainer/dev/update-bashrc.sh
+++ .devcontainer/dev/update-bashrc.sh
@@ -4,9 +4,13 @@ echo "if [ -f $HOME/.aliases.bash ]; then . $HOME/.aliases.bash; fi" >> $HOME/.b
 echo "if [ -f $HOME/.functions.bash ]; then . $HOME/.functions.bash; fi" >> $HOME/.bashrc
 
 echo "" >> $HOME/.bashrc
+echo "# add Python bin to PATH" >> $HOME/.bashrc
+echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
+
+echo "" >> $HOME/.bashrc
 echo "# setup ROS environment" >> $HOME/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> $HOME/.bashrc
 echo "export _colcon_cd_root=$ROS_WORKSPACE" >> $HOME/.bashrc
 echo "unset ROS_DISTRO  # unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> $HOME/.bashrc
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> $HOME/.bashrc
-echo "alias srcros=\"if [ -f $ROS_WORKSPACE/install/local_setup.bash ]; then source $ROS_WORKSPACE/install/local_setup.bash; echo 'sourcing the ROS overlay of $ROS_WORKSPACE'; else echo '$ROS_WORKSPACE/install/local_setup.bash not found: run the Build task (CTRL+SHIFT+B) then srcnew (in the terminal)'; fi\"" >> $HOME/.bashrc
+echo "alias srcros=\"if [ -f $ROS_WORKSPACE/install/local_setup.bash ]; then source $ROS_WORKSPACE/install/local_setup.bash; echo 'sourcing the ROS overlay of $ROS_WORKSPACE'; else echo '$ROS_WORKSPACE/install/local_setup.bash not found: run the Build task (CTRL+SHIFT+B) then srcros (in the terminal)'; fi\"" >> $HOME/.bashrc
