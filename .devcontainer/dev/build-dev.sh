ef30fa8b217c4f1a70772c0825d462ac04dbadb5
Patrick Creighton <pcreighton429@gmail.com>
2023-01-19 13:05:47 -0800

    Remove build-dev.sh, as dev image is built by ci

--- .devcontainer/dev/build-dev.sh
+++
@@ -1,7 +0,0 @@
-# Currently not used, use build dev image CI instead
-# #!/bin/bash
-
-# docker build . \
-#     --file dev.Dockerfile \
-#     --tag ghcr.io/ubcsailbot/sailbot_workspace/dev:ros_humble-ompl_d8375d8-amd64 \
-#     --build-arg ARCH=amd64/
