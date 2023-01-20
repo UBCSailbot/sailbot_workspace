c919f95184808dcb24c6c21e33331e1496935ba6
Patrick Creighton <pcreighton429@gmail.com>
2023-01-19 17:45:18 -0800

    Fix multiline command

--- .devcontainer/base/build-base.sh
+++ .devcontainer/base/build-base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker buildx build . \
-    --file base.Dockerfile
+    --file base.Dockerfile \
     --tag ghcr.io/ubcsailbot/sailbot_workspace/base:ros_humble-ompl_1bb0aa2 \
     --platform linux/arm64/v8,linux/amd64 \
     --push
