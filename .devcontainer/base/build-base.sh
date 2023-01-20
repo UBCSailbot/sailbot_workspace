4f5ceec8a392994a5be658c05f9cc4c5006850ba
Patrick Creighton <pcreighton429@gmail.com>
2023-01-19 19:19:17 -0800

    Fix platform

--- .devcontainer/base/build-base.sh
+++ .devcontainer/base/build-base.sh
@@ -3,5 +3,5 @@
 docker buildx build . \
     --file base.Dockerfile \
     --tag ghcr.io/ubcsailbot/sailbot_workspace/base:ros_humble-ompl_1bb0aa2 \
-    --platform linux/arm64/v8,linux/amd64 \
+    --platform linux/arm64,linux/amd64 \
     --push
