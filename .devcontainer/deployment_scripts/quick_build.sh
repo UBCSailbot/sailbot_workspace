e1430490ae5b228075ed75da4720c823d1fd6948
hhenry01 <henryhuang2001@gmail.com>
2023-02-26 04:46:55 +0000

    Fix quick build colcon path bug

--- .devcontainer/deployment_scripts/quick_build.sh
+++ .devcontainer/deployment_scripts/quick_build.sh
@@ -4,4 +4,7 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 HOST_WORKSPACE_ROOT="$SCRIPT_DIR/../.."
 
-$HOST_WORKSPACE_ROOT/build.sh RelWithDebInfo OFF OFF
+# colcon doesn't like to be run outside the root dir so cd into it
+cd $HOST_WORKSPACE_ROOT
+./build.sh RelWithDebInfo OFF OFF
+cd - > /dev/null
