05927ca2231bb0f7a99a6245c1e9494cf9a7e2e1
hhenry01 <henryhuang2001@gmail.com>
2023-03-04 16:30:06 +0000

    Remove redundant setup

--- .devcontainer/deployment/start_container.sh
+++ .devcontainer/deployment/start_container.sh
@@ -2,8 +2,6 @@
 # Run the base image without VSCode for deployment
 # Runs the base image used by the Dev Container by default
 # (Optional) run this script with an image ID as the first argument to run a specific version of the image
-#
-# If any CAN devices can0 or vcan0 are setup in the host machine, this script enables them inside the container
 
 IMAGE_ID=${1:-""}
 
@@ -42,50 +40,12 @@ get_dev_tag()
     echo $TAG # return tag
 }
 
-# Function to fork off and setup CAN devices
-# I do not recommend printing to the terminal inside this function as it will print onto the interactive command line
-setup_can_fork()
-{
-    # Takes a CAN device ID as the first input argument and returns the command to set it up within the docker container
-    setup_can_cmd()
-    {
-        DEV_ID=$1
-        if [[ $DEV_ID == "vcan0" ]]
-        then
-            TYPE_ARG="vcan"
-        elif [[ $DEVID == "can0" ]]
-        then
-            TYPE_ARG="can bitrate 12500" # TODO: No clue what the bitrate should be as physical CAN is not setup yet
-        fi
-        # return cmd
-        echo "docker exec -d $CONTAINER_NAME ip link add dev $DEV_ID type $TYPE_ARG && ip link set up $DEV_ID"
-    }
-
-    PARENT_PID=$$
-    # Wait for the container to start in the parent process
-    while [[ $(docker ps --filter "name=$CONTAINER_NAME" --filter status=running | grep -w $CONTAINER_NAME) == "" ]]
-    do
-        # if the parent process dies then exit
-        if [[ $(ps -p $PARENT_PID | grep -w $PARENT_PID) == "" ]]
-        then
-            exit 1
-        fi
-        sleep 1
-    done
-
-    if [[ $(ip a | grep -w vcan0) != "" ]]
-    then
-        $(setup_can_cmd vcan0)
-    fi
-    if [[ $(ip a | grep -w can0) != "" ]]
-    then
-        $(setup_can_cmd can0)
-    fi
-}
-
-# Stop any currently running or exited container that was started by this script
-docker stop $CONTAINER_NAME > /dev/null 2>&1
-docker rm $CONTAINER_NAME > /dev/null 2>&1
+if [[ $(docker ps -a --filter "name=$CONTAINER_NAME" | grep -w $CONTAINER_NAME) != "" ]]
+then
+    echo "Error: $CONTAINER_NAME is already running or suspended. Run the following command and then try again:"
+    echo "docker stop $CONTAINER_NAME; docker rm $CONTAINER_NAME"
+    exit 1
+fi
 
 DOCKER_RUN_ARGS=$(get_docker_run_args)
 DOCKER_RUN_CMD="$DOCKER_RUN_CMD $DOCKER_RUN_ARGS"
@@ -97,10 +57,6 @@ then
 else
     DOCKER_RUN_CMD="$DOCKER_RUN_CMD $IMAGE_ID /bin/bash"
 fi
-
-# Setup CAN device(s) by forking off from this script, waiting for $DOCKER_RUN_CMD to start
-# the container, and executing the commands to setup the device(s)
-setup_can_fork &
 
 echo "Running: $DOCKER_RUN_CMD"
 $DOCKER_RUN_CMD # Any commands placed after this will run AFTER the container is exited
