5e689cd6ae6346426c249d402528f7f4c2b00fce
hhenry01 <henryhuang2001@gmail.com>
2023-04-08 17:29:06 +0000

    Ignore vitual_iridium in build

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ BUILD_TYPE=${1:-RelWithDebInfo}
 STATIC_ANALYSIS=${2:-ON}
 UNIT_TEST=${3:-ON}
 colcon build \
+        --packages-ignore virtual_iridium \
         --merge-install \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DSTATIC_ANALYSIS=$STATIC_ANALYSIS" "-DUNIT_TEST=$UNIT_TEST" "--no-warn-unused-cli"
