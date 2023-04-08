41c24dd58c03b30f78d01eed7acfacd42bd79047
Henry Huang <69825683+hhenry01@users.noreply.github.com>
2023-04-08 14:05:46 -0400

    Setup virtual iridium (#112)
    * Setup virtual iridium
    
    * Update github quirks
    
    * Update virtual_iridium branch
    
    * Ignore vitual_iridium in build
    
    * Add more colcon ignores

--- .github/actions/ament-lint/run.sh
+++ .github/actions/ament-lint/run.sh
@@ -3,4 +3,7 @@ set -e
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
 ./setup.sh
-ament_${LINTER} src/
+cd src
+# exclude virtual_iridium because it is a legacy library
+VALID_SRC_DIRS=$(ls | grep -v -e virtual_iridium -e new_project.repos)
+ament_${LINTER} $VALID_SRC_DIRS
