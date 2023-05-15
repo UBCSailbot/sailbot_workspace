9717e1fa3c242adef58f32d9cb922a861d27bb54
DFriend01 <devon.fri.2000@gmail.com>
2023-05-15 02:16:15 +0000

    Update setup script for website repos

--- setup.sh
+++ setup.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 set -e
 
-if [[ $DISABLE_VCS != "true" ]]
-then
+# Display warning message
+function warn() {
+    message=$1
+    echo -e "\e[1;33m${message}\e[0m"
+}
+
+# Dependency installation for websites
+function installWebsiteDependencies() {
+    # Install dependencies for docs website
+    echo "Installing docs website Python dependencies..."
+    pip3 install -Ur websites/docs/docs/requirements.txt --upgrade-strategy eager
+}
+
+# Import all project repositories
+if [[ $DISABLE_VCS != "true" ]]; then
+    echo "Importing project repositories..."
     vcs import < src/new_project.repos src
+    vcs import < websites/web.repos websites
+    installWebsiteDependencies
+else
+    warn "VCS disabled. Skipping project repository imports..."
 fi
+
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
