b254a80e3154f9f5b1fb83c9a94ec7b28d0acf8d
DFriend01 <devon.fri.2000@gmail.com>
2023-05-27 23:27:56 +0000

    Refactor run.sh for ament-lint action to condense logic

--- .github/actions/ament-lint/run.sh
+++ .github/actions/ament-lint/run.sh
@@ -13,47 +13,27 @@ function warn {
     echo -e "\e[1;33m${message}\e[0m"
 }
 
-function do_lint_cmake {
-    VALID_SRC_DIRS_ARG=$1
-    LINTED_FILES=`find ${VALID_SRC_DIRS_ARG} -type f \( -name "CMakeLists.txt" -o -name "*.cmake" -o -name "*.cmake.in" \)`
+function get_search_command {
+    case ${LINTER} in
+        lint_cmake) CMD='find ${VALID_SRC_DIRS_ARG} -type f \( -name "CMakeLists.txt" -o -name "*.cmake" -o -name "*.cmake.in" \)' ;;
+        flake8) CMD='find ${VALID_SRC_DIRS_ARG} -type f -name "*.py"' ;;
+        pep257) CMD='find ${VALID_SRC_DIRS_ARG} -type f -name "*.py"' ;;
+        xmllint) CMD='find ${VALID_SRC_DIRS_ARG} -type f -name "*.xml"' ;;
+        *) error "ERROR: Invalid linter ${LINTER} specified in ament-lint action"; exit 1 ;;
+    esac
 
-    if [[ ! -z ${LINTED_FILES} ]]; then
-        ament_lint_cmake $LINTED_FILES
-    else
-        warn "WARNING: No files found for lint_cmake. Skipping lint_cmake..."
-    fi
+    echo $CMD
 }
 
-function do_flake8 {
+function lint {
     VALID_SRC_DIRS_ARG=$1
-    LINTED_FILES=`find ${VALID_SRC_DIRS_ARG} -type f -name "*.py"`
+    FILE_SEARCH_CMD=`get_search_command`
+    LINTED_FILES=`eval ${FILE_SEARCH_CMD}`
 
     if [[ ! -z ${LINTED_FILES} ]]; then
-        ament_flake8 $LINTED_FILES
+        ament_${LINTER} ${LINTED_FILES}
     else
-        warn "WARNING: No files found for flake8. Skipping flake8..."
-    fi
-}
-
-function do_pep257 {
-    VALID_SRC_DIRS_ARG=$1
-    LINTED_FILES=`find ${VALID_SRC_DIRS_ARG} -type f -name "*.py"`
-
-    if [[ ! -z ${LINTED_FILES} ]]; then
-        ament_pep257 $LINTED_FILES
-    else
-        warn "WARNING: No files found for pep257. Skipping pep257..."
-    fi
-}
-
-function do_xmllint {
-    VALID_SRC_DIRS_ARG=$1
-    LINTED_FILES=`find ${VALID_SRC_DIRS_ARG} -type f -name "*.xml"`
-
-    if [[ ! -z ${LINTED_FILES} ]]; then
-        ament_xmllint $LINTED_FILES
-    else
-        warn "WARNING: No files found for xmllint. Skipping xmllint..."
+        warn "WARNING: No files found for ${LINTER}. Skipping ament_${LINTER}..."
     fi
 }
 
@@ -63,10 +43,4 @@ cd src
 
 # exclude virtual_iridium because it is a legacy library
 VALID_SRC_DIRS=$(ls | grep -v -e virtual_iridium -e new_project.repos)
-case ${LINTER} in
-    lint_cmake) do_lint_cmake $VALID_SRC_DIRS ;;
-    flake8) do_flake8 $VALID_SRC_DIRS ;;
-    pep257) do_pep257 $VALID_SRC_DIRS ;;
-    xmllint) do_xmllint $VALID_SRC_DIRS ;;
-    *) error "ERROR: Invalid linter ${LINTER} specified in ament-lint action"; exit 1 ;;
-esac
+lint ${VALID_SRC_DIRS}
