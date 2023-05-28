914e4b1632da2c9cdfaa1705a42d71528a0beb41
DFriend01 <devon.fri.2000@gmail.com>
2023-05-28 00:15:05 +0000

    Remove debugging statements from setup script

--- setup.sh
+++ setup.sh
@@ -13,9 +13,6 @@ if [[ $DISABLE_VCS != "true" ]]; then
     vcs import < src/new_project.repos src
 else
     warn "VCS disabled. Skipping project repository imports..."
-    warn "SRC DIRS"
-    warn `ls src`
-    warn `pwd`
 fi
 
 sudo apt-get update
