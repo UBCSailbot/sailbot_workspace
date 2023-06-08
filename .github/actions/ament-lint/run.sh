133c109a5168908019c2d6f15afbb4cf7b99bab6
Patrick Creighton <pcreighton429@gmail.com>
2023-06-08 04:07:07 -0700

    Ignore raye-local-pathfinding in ament_flake8

--- .github/actions/ament-lint/run.sh
+++ .github/actions/ament-lint/run.sh
@@ -5,5 +5,5 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 ./setup.sh
 cd src
 # exclude virtual_iridium because it is a legacy library
-VALID_SRC_DIRS=$(ls | grep -v -e virtual_iridium -e new_project.repos)
+VALID_SRC_DIRS=$(ls | grep -v -e virtual_iridium -e raye-local-pathfinding -e new_project.repos)
 ament_${LINTER} $VALID_SRC_DIRS
