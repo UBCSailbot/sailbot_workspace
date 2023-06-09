46b4034e350a70aeb197fcdcfcda20b9e416b53d
DFriend01 <devon.fri.2000@gmail.com>
2023-06-09 00:36:58 -0700

    Remove check for files in clang-tidy

--- run_clang-tidy.sh
+++ run_clang-tidy.sh
@@ -1,5 +1,2 @@
 files=$(find ./src/network_systems/ -type f \( -iname \*.cpp -o -iname \*.h \))
-
-if [[ ! -z ${files} ]]; then
-    clang-tidy -p ./build/ --format-style=file $files
-fi
+clang-tidy -p ./build/ --format-style=file $files
