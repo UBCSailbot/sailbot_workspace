83c43fceef28f8cc6123f05022215bc66dbeb131
Patrick Creighton <pcreighton429@gmail.com>
2023-06-10 10:26:13 -0700

    Show test output

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 set -e
 
 if [ -f install/local_setup.bash ]; then source install/local_setup.bash; fi
-colcon test --packages-ignore virtual_iridium --merge-install
+colcon test --packages-ignore virtual_iridium --merge-install --event-handlers console_cohesion+
 colcon test-result
