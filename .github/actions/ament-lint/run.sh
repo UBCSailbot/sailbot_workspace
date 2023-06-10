c204d35a0fbca2cc6450f538488430e4239295aa
Devon Friend <69879126+DFriend01@users.noreply.github.com>
2023-06-10 15:43:55 -0700

    Reusable workflow optimizations with arguments and linting actions (#129)
    * Add more arguments to test definitions and apply them to job steps
    
    * Remove matrix
    
    * Revert to matrix implementation and split linting up into multiple jobs
    
    * Remove languages as required inputs to workflow
    
    * Add quotes around linter names
    
    * Tweak if statement
    
    * New multiline if statement syntax
    
    * Stuff
    
    * Separated linters into their own jobs
    
    * Change if statements
    
    * Debugging setup script
    
    * Dubugging
    
    * Debugging
    
    * Check for valid files before linting
    
    * Stop clang-tidy if no files are found
    
    * Tweak linting functions
    
    * Revert to old ament-lint job implementation and remove language inputs
    
    * Revert test.yml
    
    * Remove uses-cpp argument from clang-tidy
    
    * Change action ref to this branch
    
    * Refactor run.sh for ament-lint action to condense logic
    
    * Change refs
    
    * Corrected refs
    
    * Corrected refs
    
    * Remove docs from valid linting directories
    
    * Remove debugging statements from setup script
    
    * Reduce arguments in workflow and substituted redundant arguments
    
    * Remove check for files in clang-tidy
    
    * Remove pep257 from linters
    
    * Add comments to bash functions
    
    * Remove custom interfaces argument
    
    * Remove disable-vcs input
    
    * Change workflow branch
    
    * Change workflow branch
    
    * Remove disable-vcs
    
    * Add ros-ci argument
    
    * Change order of cloning src repos
    
    * Revert
    
    * Comment out custom interfaces
    
    * Change vcs to main
    
    * Revert
    
    * Fix indentation
    
    * Add debug step
    
    * Fix if condition
    
    * Remove debug step
    
    * Call locally defined workflow
    
    * Revamp reusable workflows documentation
    
    * Test running rebuild docs
    
    * Update gh-token documentation
    
    * Test not defining gh-token
    
    * Revert tests
    
    * Reorganize ament-lint  to match others
    
    * Testing when DISABLE_VCS should evaluate to true
    
    * Revert test
    
    ---------
    
    Co-authored-by: Patrick Creighton <pcreighton429@gmail.com>

--- .github/actions/ament-lint/run.sh
+++ .github/actions/ament-lint/run.sh
@@ -1,9 +1,51 @@
 #!/bin/bash
 set -e
 
+function error {
+    # Display error message
+    message=$1
+    echo -e "\e[1;31m${message}\e[0m"
+}
+
+function warn {
+    # Display warning message
+    message=$1
+    echo -e "\e[1;33m${message}\e[0m"
+}
+
+function get_search_command {
+    # Returns a search command in the form of a string that performs a search for
+    # files to lint. The returned search command depends on the specified linter.
+
+    case ${LINTER} in
+        lint_cmake) CMD='find ${VALID_SRC_DIRS_ARG} -type f \( -name "CMakeLists.txt" -o -name "*.cmake" -o -name "*.cmake.in" \)' ;;
+        flake8) CMD='find ${VALID_SRC_DIRS_ARG} -type f -name "*.py"' ;;
+        xmllint) CMD='find ${VALID_SRC_DIRS_ARG} -type f -name "*.xml"' ;;
+        *) error "ERROR: Invalid linter ${LINTER} specified in ament-lint action"; exit 1 ;;
+    esac
+
+    echo $CMD
+}
+
+function lint {
+    # Run a specified linter on a set of files
+    # Arg 1: A list of source directories and files to perform linting on
+
+    VALID_SRC_DIRS_ARG=$1
+    FILE_SEARCH_CMD=`get_search_command`
+    LINTED_FILES=`eval ${FILE_SEARCH_CMD}`
+
+    if [[ ! -z ${LINTED_FILES} ]]; then
+        ament_${LINTER} ${LINTED_FILES}
+    else
+        warn "WARNING: No files found for ${LINTER}. Skipping ament_${LINTER}..."
+    fi
+}
+
 source /opt/ros/${ROS_DISTRO}/setup.bash
 ./setup.sh
 cd src
-# exclude virtual_iridium because it is a legacy library
-VALID_SRC_DIRS=$(ls | grep -v -e virtual_iridium -e raye-local-pathfinding -e new_project.repos)
-ament_${LINTER} $VALID_SRC_DIRS
+
+# Exclude repos and files we don't want to lint
+VALID_SRC_DIRS=$(ls | grep -v -e virtual_iridium -e docs -e raye-local-pathfinding -e new_project.repos)
+lint ${VALID_SRC_DIRS}
--- run_clang-tidy.sh
+++ run_clang-tidy.sh
@@ -1,3 +1,2 @@
 files=$(find ./src/network_systems/ -type f \( -iname \*.cpp -o -iname \*.h \))
-
 clang-tidy -p ./build/ --format-style=file $files
