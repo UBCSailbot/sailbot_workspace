de36c3c5ba52c3255dadaf574134d6d90cbafd41
Devon Friend <69879126+DFriend01@users.noreply.github.com>
2023-06-10 19:44:43 -0700

    Add Boat Simulator and Enhance Build Tasks (#114)
    * Add boat simulator to project repos
    
    * Add optional argument to build script to specify packages to build
    
    * Add Build All and Build tasks. Build All is the old Build task
    
    * Alphabetize explorer folders
    
    * Rename build tasks and set only one as default
    
    * Alphabetize task inputs
    
    * Remove packages argument and change Build Package task to only build one package
    
    * Reconfigure build tasks
    
    * Change build documentation
    
    * Fix indentation
    
    * Remove branch references to add_lang_ci_args
    
    * Add quick build tasks to sailbot workspace
    
    * Remove redundant documentation from tests.yml
    
    * Change argument order in build.sh
    
    * Interactively select package and quick build
    
    * Update clone repos task
    
    * Convert indentations to spaces
    
    * Add back documentation
    
    * Remove unused task input
    
    ---------
    
    Co-authored-by: Patrick Creighton <pcreighton429@gmail.com>

--- build.sh
+++ build.sh
@@ -1,11 +1,50 @@
 #!/bin/bash
 set -e
 
-# Set the build type
-BUILD_TYPE=${1:-RelWithDebInfo}
-STATIC_ANALYSIS=${2:-ON}
-UNIT_TEST=${3:-ON}
+# Display a help message for using this script
+function helpMessage() {
+    echo -e "Build ROS package(s) in the Sailbot Workspace."
+    echo -e "Usage: ./build.sh [OPTION] ..."
+    echo -e "Example: ./build.sh -q -p local_pathfinding\n"
+    echo -e "Options (All Optional):"
+    echo -e "\t-q: Do a quick build. Specifying this argument will skip static analysis and unit testing when building."
+    echo -e "\t-p <PACKAGE_NAME>: Build a ROS package. Can only specify one package. If argument not included, all packages are built."
+    echo -e "\t-h: Display this message."
+}
+
+# Package to build if selecting an individual package
+# If still empty after argument parsing, all ROS packages are built
+PACKAGE=""
+
+# Boolean for quick build (default is full build)
+QUICK_BUILD="false"
+
+# Parse command-line options (all are optional arguments)
+while getopts "hp:q" flag; do
+    case ${flag} in
+        p )     PACKAGE="${OPTARG}" ;;
+        q )     QUICK_BUILD="true" ;;
+        h )     helpMessage; exit 0 ;;
+        \? )    echo "Invalid option: -${flag}"; helpMessage; exit 1 ;;
+        : )     echo "Option -${flag} requires an argument"; helpMessage; exit 1 ;;
+    esac
+done
+
+# Assign build type default
+BUILD_TYPE="RelWithDebInfo"
+
+# Configuration for build (full or quick)
+if [[ $QUICK_BUILD = "true" ]]; then
+    STATIC_ANALYSIS="OFF"
+    UNIT_TEST="OFF"
+else
+    STATIC_ANALYSIS="ON"
+    UNIT_TEST="ON"
+fi
+
+# Build ROS packages in src directory
 colcon build \
+        ${PACKAGE:+--packages-select $PACKAGE} \
         --packages-ignore virtual_iridium \
         --merge-install \
         --symlink-install \
