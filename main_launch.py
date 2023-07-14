5d4041b9b0298e3d95b08788bced60190f24bc19
Patrick Creighton <pcreighton429@gmail.com>
2023-07-14 15:17:30 -0700

    Format main_launch.py

--- main_launch.py
+++ main_launch.py
@@ -1,12 +1,16 @@
 import os
 from collections import namedtuple
-from typing import List, Dict
+from typing import Dict, List
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
 from launch.launch_context import LaunchContext
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-
 
 # TODO: Add the controller package when it is ready
 ROS_PACKAGES_DIR = os.path.join(os.getenv("ROS_WORKSPACE"), "src")
@@ -26,10 +30,9 @@ LAUNCH_MODES = ["production", "simulation"]
 
 def generate_launch_description() -> LaunchDescription:
     declared_launch_arguments_list = generate_launch_argument_declarations()
-    launch_description = LaunchDescription([
-        *declared_launch_arguments_list,
-        OpaqueFunction(function=launch_setup)
-    ])
+    launch_description = LaunchDescription(
+        [*declared_launch_arguments_list, OpaqueFunction(function=launch_setup)]
+    )
     return launch_description
 
 
@@ -37,33 +40,32 @@ def generate_launch_argument_declarations() -> List[DeclareLaunchArgument]:
     declared_arguments = list()
     for argument in LAUNCH_ARGUMENTS:
         declared_argument = DeclareLaunchArgument(
-            name=argument.name,
-            default_value=argument.default,
-            description=argument.description
+            name=argument.name, default_value=argument.default, description=argument.description
         )
         declared_arguments.append(declared_argument)
     return declared_arguments
 
 
-def launch_setup(context : LaunchContext, *args, **kwargs) -> List[IncludeLaunchDescription]:
+def launch_setup(context: LaunchContext, *args, **kwargs) -> List[IncludeLaunchDescription]:
     launch_args = parse_launch_args(context=context)
     ros_package_list = get_running_ros_packages(mode=launch_args["mode"])
     include_launch_descriptions = get_include_launch_descriptions(ros_packages=ros_package_list)
     return include_launch_descriptions
 
 
-def parse_launch_args(context : LaunchContext) -> Dict[str, str]:
+def parse_launch_args(context: LaunchContext) -> Dict[str, str]:
     parsed_args = dict()
     for argument in LAUNCH_ARGUMENTS:
         parsed_args[argument.name] = LaunchConfiguration(argument.name).perform(context)
     return parsed_args
 
 
-def get_include_launch_descriptions(ros_packages : List[str]) -> List[IncludeLaunchDescription]:
+def get_include_launch_descriptions(ros_packages: List[str]) -> List[IncludeLaunchDescription]:
     package_include_launch_descriptions = list()
     for package in ros_packages:
-        launch_file_abs_path = os.path.join(ROS_PACKAGES_DIR, package, 'launch', package) +\
-            LAUNCH_FILENAME_ENDING
+        launch_file_abs_path = (
+            os.path.join(ROS_PACKAGES_DIR, package, 'launch', package) + LAUNCH_FILENAME_ENDING
+        )
         include_launch_description = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(launch_file_abs_path)
         )
@@ -71,7 +73,7 @@ def get_include_launch_descriptions(ros_packages : List[str]) -> List[IncludeLau
     return package_include_launch_descriptions
 
 
-def get_running_ros_packages(mode : str) -> List[str]:
+def get_running_ros_packages(mode: str) -> List[str]:
     match mode:
         case "production":
             return PRODUCTION_ROS_PACKAGES
