79fff20b16ba34e801c1f6d4d92e4d4146fd508c
DFriend01 <devon.fri.2000@gmail.com>
2023-07-15 00:51:48 -0700

    Add documentation to functions and remove unnecessary logic

--- main_launch.py
+++ main_launch.py
@@ -1,5 +1,4 @@
 import os
-from collections import namedtuple
 from typing import Dict, List
 
 from launch import LaunchDescription
@@ -21,32 +20,38 @@ LAUNCH_FILENAME_ENDING = "_launch.py"
 # Add launch arguments here and edit launch_setup() to use new argument(s)
 # if needed for global setup.
 # TODO: Pass global arguments to children launch files.
-LaunchArgument = namedtuple("LaunchArgument", ["name", "default", "description"])
-LAUNCH_ARGUMENTS = [
-    LaunchArgument("mode", "production", "System mode. One of 'production', 'simulation'.")
-]
 LAUNCH_MODES = ["production", "simulation"]
+LAUNCH_ARGUMENTS = [
+    DeclareLaunchArgument(
+        name="mode", default_value="production", choices=LAUNCH_MODES, description="System mode."
+    )
+]
 
 
 def generate_launch_description() -> LaunchDescription:
-    declared_launch_arguments_list = generate_launch_argument_declarations()
-    launch_description = LaunchDescription(
-        [*declared_launch_arguments_list, OpaqueFunction(function=launch_setup)]
-    )
+    """Entry point for the launch file. Invokes launch files from all workspace ROS packages.
+
+    Returns:
+        LaunchDescription: A collection of entities representing the behavior of the system.
+    """
+    launch_description = LaunchDescription([
+        *LAUNCH_ARGUMENTS,
+        OpaqueFunction(function=launch_setup)
+    ])
     return launch_description
 
 
-def generate_launch_argument_declarations() -> List[DeclareLaunchArgument]:
-    declared_arguments = list()
-    for argument in LAUNCH_ARGUMENTS:
-        declared_argument = DeclareLaunchArgument(
-            name=argument.name, default_value=argument.default, description=argument.description
-        )
-        declared_arguments.append(declared_argument)
-    return declared_arguments
-
-
 def launch_setup(context: LaunchContext, *args, **kwargs) -> List[IncludeLaunchDescription]:
+    """Gathers launch descriptions from the workspace ROS packages to launch. Invoking this
+    function from an OpaqueFunction object allows for immediate access to ROS parameters passed
+    to this launch file.
+
+    Args:
+        context (LaunchContext): The runtime context from the entity invoking this function.
+
+    Returns:
+        List[IncludeLaunchDescription]: A list of launch descriptions from ROS packages to be run.
+    """
     launch_args = parse_launch_args(context=context)
     ros_package_list = get_running_ros_packages(mode=launch_args["mode"])
     include_launch_descriptions = get_include_launch_descriptions(ros_packages=ros_package_list)
@@ -54,6 +59,15 @@ def launch_setup(context: LaunchContext, *args, **kwargs) -> List[IncludeLaunchD
 
 
 def parse_launch_args(context: LaunchContext) -> Dict[str, str]:
+    """Parses ROS arguments from a runtime context.
+
+    Args:
+        context (LaunchContext): The context to be parsed.
+
+    Returns:
+        Dict[str, str]: A dict containing arguments, where they keys are argument names and the
+        values are the argument values.
+    """
     parsed_args = dict()
     for argument in LAUNCH_ARGUMENTS:
         parsed_args[argument.name] = LaunchConfiguration(argument.name).perform(context)
@@ -61,6 +75,14 @@ def parse_launch_args(context: LaunchContext) -> Dict[str, str]:
 
 
 def get_include_launch_descriptions(ros_packages: List[str]) -> List[IncludeLaunchDescription]:
+    """Construct include launch descriptions for the indicated ROS packages.
+
+    Args:
+        ros_packages (List[str]): List of ROS packages names to be included.
+
+    Returns:
+        List[IncludeLaunchDescription]: List of include launch descriptons for each ROS package.
+    """
     package_include_launch_descriptions = list()
     for package in ros_packages:
         launch_file_abs_path = (
@@ -74,6 +96,17 @@ def get_include_launch_descriptions(ros_packages: List[str]) -> List[IncludeLaun
 
 
 def get_running_ros_packages(mode: str) -> List[str]:
+    """Gets the names of the ROS packages to be launched depending on an indicated mode.
+
+    Args:
+        mode (str): The system mode upon launch. Must be one of 'production', 'simulation'.
+
+    Raises:
+        ValueError: Raised when an invalid mode is passed.
+
+    Returns:
+        List[str]: List of ROS package names to be launched.
+    """
     match mode:
         case "production":
             return PRODUCTION_ROS_PACKAGES
