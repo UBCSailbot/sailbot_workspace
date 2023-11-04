dfa1e3c38e015854a331efe85131b499ccc0dd19
Henry Huang <69825683+hhenry01@users.noreply.github.com>
2023-11-04 00:28:13 -0700

    Start and kill virtual iridium in test.sh (#187)

--- test.sh
+++ test.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+function signal_handler() {
+    if [ -n "$(pgrep -f virtual_iridium)" ]; then
+        pkill -f virtual_iridium
+    fi
+}
+
+# Test failures terminate the script
+# Catch the signal so child processes can be cleaned up
+trap 'exit' INT TERM
+trap 'signal_handler' EXIT
+
 if [ -f install/local_setup.bash ]; then source install/local_setup.bash; fi
+if [ -d src/network_systems ]; then src/network_systems/scripts/run_virtual_iridium.sh &> /dev/null & fi
 colcon test --packages-ignore virtual_iridium --merge-install --event-handlers console_cohesion+
 colcon test-result
+exit 0
