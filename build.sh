bdb34d8a99bc81abeed83c4ef1ce4f6fe37d512a
hhenry01 <henryhuang2001@gmail.com>
2023-11-08 12:35:49 -0800

    Fix cpp config

--- build.sh
+++ build.sh
@@ -41,9 +41,3 @@ colcon build \
         --merge-install \
         --symlink-install \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DSTATIC_ANALYSIS=$STATIC_ANALYSIS" "-DUNIT_TEST=$UNIT_TEST" "--no-warn-unused-cli"
-
-# For Clangd
-if [[ -f $ROS_WORKSPACE/build/compile_commands.json && ! -f $ROS_WORKSPACE/compile_commands.json ]]
-then
-    ln -s $ROS_WORKSPACE/build/compile_commands.json $ROS_WORKSPACE
-fi
