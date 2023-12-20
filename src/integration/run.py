b973b033fd29b792284f351166628f30ea7df6eb
Patrick Creighton <pcreighton429@gmail.com>
2023-12-20 15:12:59 -0800

    Fix flake8 errors

--- src/integration/run.py
+++ src/integration/run.py
@@ -4,25 +4,23 @@ import signal
 import subprocess
 import sys
 import time
+
 import yaml
 
 ROS_LAUNCH_CMD = "ros2 launch {} main_launch.py"
-ROS_PACKAGES = [
-    "boat_simulator",
-    "controller",
-    "local_pathfinding",
-    "network_systems"
-]
+ROS_PACKAGES = ["boat_simulator", "controller", "local_pathfinding", "network_systems"]
 ROS_WORKSPACE_PATH = os.getenv("ROS_WORKSPACE", default="/workspaces/sailbotworkspace")
+
 
 def get_ros_launch_cmd(package_name, launch_config_files):
     launch_cmd = ROS_LAUNCH_CMD.format(package_name)
 
     if launch_config_files is not None:
-        config_files_str = ''.join(launch_config_files)
-        launch_cmd += ' --config:={}'.format(config_files_str)
+        config_files_str = "".join(launch_config_files)
+        launch_cmd += " --config:={}".format(config_files_str)
 
     return launch_cmd
+
 
 def launch_modules(packages):
     launch_cmds = list()
@@ -37,29 +35,40 @@ def launch_modules(packages):
                 case "website":
                     pass
                 case _:
-                    sys.exit("Error, invalid package name: {}".format(package_name))
+                    sys.exit("Error, invalid package name: {}".format(package["name"]))
 
     procs = list()
     for cmd in launch_cmds:
-        procs.append(subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)) # Use os.setsid to set process group
+        procs.append(
+            subprocess.Popen(
+                cmd,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.PIPE,
+                shell=True,
+                preexec_fn=os.setsid,
+            )
+        )  # Use os.setsid to set process group
     return procs
+
 
 def stop_modules(process_list):
     for proc in process_list:
         os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', required=True, metavar='path', dest='config_file')
+    parser.add_argument("-c", "--config", required=True, metavar="path", dest="config_file")
     args = parser.parse_args()
 
-    with open(args.config_file, 'r') as config_file:
+    with open(args.config_file, "r") as config_file:
         params = yaml.safe_load(config_file)
 
         procs = launch_modules(params["required_packages"])
 
         time.sleep(10)
         stop_modules(procs)
+
 
 if __name__ == "__main__":
     main()
