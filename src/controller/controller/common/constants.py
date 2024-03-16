ae4e39247fc58a25e984d1e2362ad8fc3527237c
Alberto <albertoescobar@live.com>
2024-03-16 16:05:31 -0700

    integrated LUT and WingsailController classes to wingsail_ctrl_node

--- src/controller/controller/common/constants.py
+++ src/controller/controller/common/constants.py
@@ -5,3 +5,14 @@ CHORD_WIDTH_MAIN_SAIL = 0.23
 
 # {m^2 / s at 10degC} and air density at 1.225 {kg / m^3}
 KINEMATIC_VISCOSITY = 0.000014207
+
+# lookup_table (List[List[Scalar]]): A list of lists or NDArray containing x-y
+# data points for interpolation. Shape should be (n, 2).
+# x units is reynolds number, y units is degrees
+REYNOLDS_NUMBER_ALPHA_TABLE = [
+    [50000, 5.75],
+    [100000, 6.75],
+    [200000, 7],
+    [500000, 9.25],
+    [1000000, 10],
+]
