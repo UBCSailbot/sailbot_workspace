a9134f015898aa59505b6e0dc31c43ca7c3952d9
Devon Friend <69879126+DFriend01@users.noreply.github.com>
2024-03-19 14:25:07 -0700

    Sensors: Add delay model (#311)
    * Migrate code for issue 308
    
    * Sensors: Separate dataline stdevs
    
    * cleanup
    
    ---------
    
    Co-authored-by: Chris Chang <64388914+chrischang5@users.noreply.github.com>
    Co-authored-by: Patrick Creighton <pcreighton429@gmail.com>
    Co-authored-by: Chris Chang <1chang1chang1@gmail.com>

--- src/boat_simulator/tests/unit/common/test_wind_sensor.py
+++ src/boat_simulator/tests/unit/common/test_wind_sensor.py
@@ -1,59 +1,29 @@
 from boat_simulator.common.sensors import WindSensor
 import numpy as np
-from boat_simulator.common.generators import (
-    MVGaussianGenerator,
-    ConstantGenerator,
-)
 
 
 class TestWindSensor:
     def test_wind_sensor_init(self):
         init_data = np.array([1, 0])
-        error_fn = None
         ws = WindSensor(
             wind=init_data,
-            wind_noisemaker=error_fn,
         )
 
-        assert ws.wind_noisemaker == error_fn
         assert np.all(ws.wind == init_data)
 
-    def test_wind_sensor_init_implicit_error_fn(self):
-        init_data = np.array([1, 0])
-        ws = WindSensor(wind=init_data)
-
-        assert ws.wind_noisemaker is None
-        assert np.all(ws.wind == init_data)
-
-    def test_wind_sensor_read_no_error(self):
+    def test_wind_sensor_read_no_noise(self):
         init_data = np.array([1, 0])
         ws = WindSensor(
             wind=init_data,
         )
         read_data = ws.read("wind")
-        assert (init_data == read_data).all()
+        assert np.all(init_data == read_data)
 
-    def test_wind_sensor_read_constant_error(self):
-        init_data = np.array([1, 0])
-        const_err = 0.1
-        error_fn = ConstantGenerator(constant=0.1)
-        ws = WindSensor(
-            wind=init_data,
-            wind_noisemaker=error_fn,
-        )
-
-        read_data = ws.read("wind")
-        assert ((init_data + const_err) == read_data).all()
-
-    def test_wind_sensor_read_mv_gaussian_error(self):
-        init_data = np.array([1, 0])
-        mean = np.array([1, 1])
+    def test_wind_sensor_read_mv_gaussian_noise(self):
+        init_data = np.array([0, 0])
+        mean = np.array([0, 0])
         cov = np.eye(2)
-        error_fn = MVGaussianGenerator(mean=mean, cov=cov)
-        ws = WindSensor(
-            wind=init_data,
-            wind_noisemaker=error_fn,
-        )
+        ws = WindSensor(wind=init_data, enable_noise=True)
 
         NUM_READINGS = 10000
         reading = np.zeros(shape=(NUM_READINGS, mean.size))
@@ -63,15 +33,35 @@ class TestWindSensor:
         sample_mean = np.mean(reading, axis=0)
         sample_cov = np.cov(reading, rowvar=False)
 
+        assert np.allclose(sample_mean, mean, atol=0.2)
         assert np.allclose(sample_cov, cov, atol=0.2)
-        assert np.isclose(sample_mean, mean + init_data, 0.1).all()
 
-    def test_wind_sensor_update(self):
-        init_data = np.zeros(2)
+    def test_wind_sensor_update_no_delay(self):
+        init_data = np.array([0, 0])
         ws = WindSensor(wind=init_data)
 
         NUM_READINGS = 100
         for i in range(NUM_READINGS):
             wind = ws.read("wind")
-            assert (wind == np.array([i, i])).all()
+            assert np.all(wind == np.array([i, i]))
             ws.update(wind=(wind + 1))
+
+    def test_wind_sensor_update_with_delay(self):
+        """
+        Attempt to constantly update wind sensor with new data.
+        Delay causes new data to be read in the next update cycle.
+        """
+
+        init_data = np.array([0, 0])
+
+        ws = WindSensor(wind=init_data, enable_delay=True)
+
+        wind = ws.read("wind")
+        # Initialized data is read without delay
+        assert np.all(wind == init_data)
+
+        NUM_UPDATES = 3
+        for i in range(NUM_UPDATES):
+            ws.update(wind=np.array([i + 1, i + 1]))
+            wind = ws.read("wind")
+            assert np.all(wind == [i, i])
