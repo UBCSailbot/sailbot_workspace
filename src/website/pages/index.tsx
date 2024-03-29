fc897125d79614d16e29dbdab2a2fac9c43fdaef
JordanChen123 <jordanthechen@gmail.com>
2024-03-28 18:17:12 -0700

    Modal Dropdown for Data Download

--- src/website/pages/index.tsx
+++ src/website/pages/index.tsx
@@ -7,17 +7,7 @@ import PolarisContainer from '@/views/components/Polaris/PolarisContainer';
 
 const MapsContainer = dynamic(() => import('@/views/MapsContainer'), {
   loading: () => (
-    <CircularProgress
-      style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        margin: 'auto',
-        height: '100px',
-        width: '100px',
-      }}
-    />
+    <CircularProgress className={styles.loadingSpinner} />
   ),
   ssr: false,
 });
@@ -40,38 +30,20 @@ export default function Home() {
           <DashboardContainer />
         </div>
       </div>
-      <div className={styles.polarisContainer}>
-        <PolarisContainer />
-      </div>
+
       <Modal
         open={isModalOpen}
         onClose={handleCloseModal}
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
-        sx={{'z-index': 20000000000}}
+        sx={{ 'z-index': 20000000000 }}
       >
-<div style={{
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '80vw', 
-  maxWidth: '800px', 
-  backgroundColor: 'white',
-  border: '2px solid #000',
-  boxShadow: '0 4px 20px rgba(0, 0, 0, 0.1)',
-  padding: '20px', 
-  overflowY: 'scroll',
-  overflowX: 'hidden',
-  maxHeight: '90vh',
-}}>
-  <div style={{ transform: 'scale(1)', transformOrigin: 'top center' }}>
-    <PolarisContainer />
-  </div>
-</div>
-
+        <div className={styles.modalContent}>
+          <div style={{ transform: 'scale(1)', transformOrigin: 'top center' }}>
+            <PolarisContainer />
+          </div>
+        </div>
       </Modal>
     </>
   );
 }
-
