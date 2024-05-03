0ba875eedb823adb82c8c63afaba1cebdf87cfb2
Chris <1chang1chang1@gmail.com>
2024-05-03 13:59:12 -0700

    Update more script paths

--- .github/actions/run-in-container/test.sh
+++ .github/actions/run-in-container/test.sh
@@ -2,6 +2,6 @@
 set -e
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
-./setup.sh
-./build.sh RelWithDebInfo OFF  # Do not run static analysis or linting
-./test.sh
+./scripts/setup.sh
+./scripts/build.sh RelWithDebInfo OFF  # Do not run static analysis or linting
+./scripts/test.sh
