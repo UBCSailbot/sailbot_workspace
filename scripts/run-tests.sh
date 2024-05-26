f3029481ed25bd4dc4d441ba58361858b8c08a6e
patrick-5546 <pcreighton429@gmail.com>
2024-05-26 16:46:06 -0700

    Remove unneeded parts of run-tests script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-source /opt/ros/${ROS_DISTRO}/setup.bash
 ./scripts/setup.sh
-./scripts/build.sh RelWithDebInfo OFF  # Do not run static analysis or linting
+./scripts/build.sh
 ./scripts/test.sh
