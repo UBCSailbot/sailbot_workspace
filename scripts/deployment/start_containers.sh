a0773346065c45e43a25ee389c2cd434c24cd721
patrick-5546 <pcreighton429@gmail.com>
2024-05-26 16:49:18 -0700

    Start revamping start container script to use docker compose

---
+++ scripts/deployment/start_containers.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# TODO: check if it works without internet once built
+# TODO: argument for deleting containers
+# TODO: argument for deleting volumes
+
+## CLI ARGUMENTS ---
+
+# default argument values
+BASE_TAG="latest"
+WEBSITE_ARG=""
+INTERACTIVE=""
+
+# Function to display usage help
+usage() {
+    echo "Usage: $0 [--tag tag] [--website]"
+    echo "  --tag          Specify the base Docker image tag (default: latest)"
+    echo "  --website      If set, runs the website container"
+    echo "  --interactive  If set, run commands inside the sailbot workspace container interactively"
+    exit 1
+}
+
+# parse command-line options
+while getopts ":-:" opt; do
+    case "${opt}" in
+        -)
+            case "${OPTARG}" in
+                tag)
+                    BASE_TAG="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+                    ;;
+                website)
+                    WEBSITE_ARG="-f .devcontainer/website/docker-compose.website.prod.yml"
+                    ;;
+                interactive)
+                    INTERACTIVE="interactive"
+                    ;;
+                *)
+                    usage  # Show usage if argument is not recognized
+                    ;;
+            esac
+            ;;
+        *)
+            usage  # Show usage if option is unknown
+            ;;
+    esac
+done
+
+
+## HELPER VARIABLES ---
+
+# get absolute path to workspace root in host OS
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+HOST_WORKSPACE_ROOT="$SCRIPT_DIR/../.."
+
+# common arguments for docker compose commmands
+DOCKER_COMPOSE_ARGS="docker compose -f .devcontainer/docker-compose.yml $WEBSITE_ARG"
+
+
+## RUN COMMANDS ---
+
+# run commands in the workspace's root directory
+cd $HOST_WORKSPACE_ROOT
+
+# start containers
+SW_TAG=$BASE_TAG $DOCKER_COMPOSE_ARGS up --detach
+
+# run commands inside sailbot workspace container
+if [[ -n $INTERACTIVE ]]; then
+    $DOCKER_COMPOSE_ARGS exec -it sailbot-workspace /bin/bash
+else
+    $DOCKER_COMPOSE_ARGS exec -T sailbot-workspace /bin/bash -c "\
+    cd \$ROS_WORKSPACE && \
+    pwd && \
+    ./scripts/setup.sh && \
+    ./scripts/build.sh && \
+    ./scripts/test.sh && \
+    ls"
+fi
+
+# stop containers
+$DOCKER_COMPOSE_ARGS stop
