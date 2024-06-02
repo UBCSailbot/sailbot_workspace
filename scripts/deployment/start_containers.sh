0aad7c93463765ead9c95c8d838a5e76cb425390
Patrick Creighton <pcreighton429@gmail.com>
2024-06-02 14:13:33 -0700

    Downgrade mongodb to be compatible with rpi

--- scripts/deployment/start_containers.sh
+++ scripts/deployment/start_containers.sh
@@ -58,7 +58,7 @@ SCRIPT_DIR="$(dirname "$(readlink --canonicalize "$0")")"
 HOST_WORKSPACE_ROOT="$SCRIPT_DIR/../.."
 
 # common arguments for docker compose commmands
-MONGO_TAG="7"
+MONGO_TAG="4.4.18"
 PROJECT_NAME="deployment"
 DOCKER_COMPOSE_ARGS="docker compose --project-name $PROJECT_NAME --file .devcontainer/docker-compose.yml $WEBSITE_ARG"
 
