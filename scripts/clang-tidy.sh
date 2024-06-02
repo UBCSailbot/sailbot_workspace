225cd62e0a6a32066138a98f8fabf9e6eda60e43
Patrick Creighton <pcreighton429@gmail.com>
2024-06-02 14:23:07 -0700

    Remove deprecated build option

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -4,7 +4,7 @@ set -e
 if [[ $LOCAL_RUN != "true" ]]; then
     source /opt/ros/${ROS_DISTRO}/setup.bash
     ./scripts/setup.sh
-    ./scripts/build.sh RelWithDebInfo OFF
+    ./scripts/build.sh
 fi
 
 python3 scripts/ament_clang_tidy.py build/compile_commands.json --jobs 8
