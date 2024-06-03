f8f05bdab0ed020afadfd8bf447af7eab74d7568
Patrick Creighton <pcreighton429@gmail.com>
2024-06-02 18:45:55 -0700

    Fiddle around with user permissions

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -2,6 +2,11 @@
 set -e
 
 if [[ $LOCAL_RUN != "true" ]]; then
+    # give user permissions
+    ls -la $ROS_WORKSPACE
+    sudo chown -R $(whoami):$(whoami) $ROS_WORKSPACE
+    ls -la $ROS_WORKSPACE
+
     source /opt/ros/${ROS_DISTRO}/setup.bash
     ./scripts/setup.sh
     ./scripts/build.sh
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# give user permissions
+ls -la $ROS_WORKSPACE
+sudo chown -R $(whoami):$(whoami) $ROS_WORKSPACE
+ls -la $ROS_WORKSPACE
+
 ./scripts/setup.sh
 ./scripts/build.sh
 ./scripts/test.sh
