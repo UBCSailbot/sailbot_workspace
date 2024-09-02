953cad7fdb9ba2d57f983f6e03749d2a4762a4ad
fyang151 <fyang151@gmail.com>
2024-09-01 17:07:36 -0700

    WIP: format selection from dropdown

--- src/website/utils/DownloadData.js
+++ src/website/utils/DownloadData.js
@@ -1,17 +1,42 @@
-const downloadSensorData = async (sensorType) => {
+const downloadSensorData = async (sensorType, format) => {
   try {
+    if(!format) {
+      return
+    }
+
     const apiUrl = `${process.env.NEXT_PUBLIC_SERVER_HOST}:${process.env.NEXT_PUBLIC_SERVER_PORT}/api/${sensorType}`;
     const response = await fetch(apiUrl);
     if (!response.ok) {
       throw new Error(`Error: ${response.statusText}`);
     }
     const { data } = await response.json();
-    const jsonStr = JSON.stringify(data);
-    const blob = new Blob([jsonStr], { type: 'application/json' });
+
+    let blob;
+    let filename = `${sensorType}.${format}`;
+
+    if (format === 'CSV') {
+      const csvRows = [];
+      const headers = Object.keys(data[0]);
+      csvRows.push(headers.join(','));
+
+      for (const row of data) {
+        const values = headers.map(header => {
+          const escaped = ('' + row[header]).replace(/"/g, '\\"');
+          return `"${escaped}"`;
+        });
+        csvRows.push(values.join(','));
+      }
+
+      const csvString = csvRows.join('\n');
+      blob = new Blob([csvString], { type: 'text/csv' });
+    } else if (format === 'JSON') {
+      const jsonStr = JSON.stringify(data);
+      blob = new Blob([jsonStr], { type: 'application/json' });
+    }
 
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = `${sensorType}.json`;
+    link.download = filename;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -20,11 +45,11 @@ const downloadSensorData = async (sensorType) => {
   }
 };
 
-export const downloadGPSData = () => downloadSensorData('gps');
-export const downloadAISShipsData = () => downloadSensorData('aisships');
-export const downloadGlobalPathData = () => downloadSensorData('globalpath');
-export const downloadLocalPathData = () => downloadSensorData('localpath');
-export const downloadBatteriesData = () => downloadSensorData('batteries');
-export const downloadWindSensorsData = () => downloadSensorData('wind-sensors');
-export const downloadGenericSensorsData = () =>
-  downloadSensorData('generic-sensors');
+export const downloadGPSData = (format) => downloadSensorData('gps', format);
+export const downloadAISShipsData = (format) => downloadSensorData('aisships', format);
+export const downloadGlobalPathData = (format) => downloadSensorData('globalpath', format);
+export const downloadLocalPathData = (format) => downloadSensorData('localpath', format);
+export const downloadBatteriesData = (format) => downloadSensorData('batteries', format);
+export const downloadWindSensorsData = (format) => downloadSensorData('wind-sensors', format);
+export const downloadGenericSensorsData = (format) =>
+  downloadSensorData('generic-sensors', format);
