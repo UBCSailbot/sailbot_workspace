fae306449b646c9bf925e4646a0207633de98aa5
kristatraboulay <144058657+kristatraboulay@users.noreply.github.com>
2025-01-11 18:03:56 -0800

    Added parameterized tests

--- src/boat_simulator/tests/unit/nodes/physics_engine/test_model.py
+++ src/boat_simulator/tests/unit/nodes/physics_engine/test_model.py
@@ -6,7 +6,6 @@ import numpy as np
 import pytest
 from numpy.typing import NDArray
 
-from boat_simulator.common.constants import BOAT_PROPERTIES
 from boat_simulator.common.types import Scalar
 from boat_simulator.nodes.physics_engine.kinematics_computation import BoatKinematics
 from boat_simulator.nodes.physics_engine.kinematics_data import KinematicsData
@@ -14,7 +13,60 @@ from boat_simulator.nodes.physics_engine.model import BoatState
 
 
 class TestBoatState:
-    # pass
+    @pytest.mark.parametrize(
+        "timestep, glo_wind_vel, glo_water_vel, rudder_angle_deg, trim_tab_angle, input_kin_data",
+        [
+            # Generic test
+            (
+                0.1,
+                np.array([-4.0, 5.5, 0.0], dtype=np.float32),
+                np.array([1.0, -2.0, 0.0], dtype=np.float32),
+                0,
+                3,
+                (
+                    np.zeros(3, dtype=np.float32),
+                    [2.0, 3.0, 0.0],
+                    [1.5, 0.1, 0],
+                    [0.2, 0.0, 3.0],
+                    [0, 0.0, 1.1],
+                    [-0.4, 0.0, 0],
+                ),
+            ),
+            # Test for still water and no wind, angular velocity, or angular acceleration
+            (
+                0.005,
+                np.array([0.0, 0.0, 0.0], dtype=np.float32),
+                np.array([0.0, 0.0, 0.0], dtype=np.float32),
+                15,
+                0,
+                (
+                    [120.0, 52.7, 0.0],
+                    [2.0, 3.0, 0.0],
+                    [-1, 2, 0],
+                    [0.2, 0.0, 0.08],
+                    np.zeros(3, dtype=np.float32),
+                    np.zeros(3, dtype=np.float32),
+                ),
+            ),
+            # Test for long timestep, large parameters, negative angles
+            (
+                8,
+                np.array([5.0, 9.2, 0.0], dtype=np.float32),
+                np.array([-4.23, 3.0, 0.0], dtype=np.float32),
+                -370,
+                -2.6,
+                (
+                    [-2000, -4000, 0.0],
+                    [6.0, 12.0, 0.0],
+                    [3, -5, 0],
+                    [0.0, 0.0, 2],
+                    [0.3, 0.0, 3],
+                    [-0.8, 0.0, 1.1],
+                ),
+            ),
+        ],
+    )
+    # Assumes __compute_net_force_and_torque and __kinematics_computation.step work
     def test_step_boat_state(
         self,
         timestep: Scalar,
@@ -24,44 +76,22 @@ class TestBoatState:
         trim_tab_angle: Scalar,
         input_kin_data: NDArray,
     ):
-        # Assumes __compute_net_force_and_torque and __kinematics_computation.step work
-        # __compute_net_force_and_torque needs to be tested
-
-        # Set initial KinematicsData variables in pytest.mark.parameterize
-        # Setting linear velocity gives the global velocity (for actual calculation)
-
-        # Set global velocity manually for expected
-
-        # net force and torque computed in BoatState.step, set manually in expected
-        # compute all other intermediate vars manually w/ dif names
-
         test_boat_state = BoatState(timestep)
+        print(dir(test_boat_state))
         input_kinematics = KinematicsData(input_kin_data)
 
         relative_wind_vel = glo_wind_vel - input_kinematics.linear_velocity
         relative_water_vel = glo_water_vel - input_kinematics.linear_velocity
 
-        total_force, total_torque = test_boat_state.__compute_net_force_and_torque(
+        total_force, total_torque = test_boat_state._BoatState__compute_net_force_and_torque(
             relative_wind_vel, relative_water_vel, rudder_angle_deg, trim_tab_angle
         )
 
         actual_step = test_boat_state.step(
             glo_wind_vel, glo_water_vel, rudder_angle_deg, trim_tab_angle
         )
-        expected_step = test_boat_state.__kinematics_computation.step(total_force, total_torque)
+        expected_step = test_boat_state._BoatState__kinematics_computation.step(
+            total_force, total_torque
+        )
 
-        assert (actual_step == expected_step)
-
-    @pytest.mark.parametrize(
-        "timestep, glo_wind_vel, glo_water_vel, rudder_angle_deg, trim_tab_angle, input_kin_data",
-        [
-            (
-                0.1,
-                np.array([4.0, 5.5, 0.0], dtype=np.float32),
-                np.array([1.0, 2.0, 0.0], dtype=np.float32),
-                15,
-                35,
-
-            )
-        ]
-    )
+        assert actual_step == expected_step
