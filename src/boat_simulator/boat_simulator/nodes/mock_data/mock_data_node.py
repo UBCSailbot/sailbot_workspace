af1574073331b8fbcde8a1fcda228a0975c1884d
Alberto Escobar <60338723+alberto-escobar@users.noreply.github.com>
2025-01-11 19:44:29 -0800

    Implemented mock data node (#453)
    * Implemented mock data node
    
    * added CLI arguement for enabling mock data node
    
    * fixed comment in constant.py and improved mock data node instantiation
    
    * added globals.yaml parameter for mock data node
    
    * corrected parameter value in globals.yaml
    
    * updated README for global_launch
    
    * resolving comments
    
    * fixing formatting on global launch README
    
    * minor formatting changes
    
    * resolving more comments
    
    * final comment resolved
    
    * final fixes
    
    * final fixes THIS TIMEE I MEAN IT

---
+++ src/boat_simulator/boat_simulator/nodes/mock_data/mock_data_node.py
@@ -0,0 +1,172 @@
+import random
+import sys
+
+import rclpy
+from custom_interfaces.msg import DesiredHeading, HelperHeading, SailCmd
+from rclpy.node import Node
+
+import boat_simulator.common.constants as Constants
+
+
+class MockDataNode(Node):
+    """the purpose of this node is to publish to all topics that there is subscribers in physics
+    engine node so send goal code can work.
+    based on following:
+    https://docs.ros.org/en/humble/Tutorials/Beginner-Client-Libraries/Writing-A-Simple-Py-Publisher-And-Subscriber.html
+
+     Publishers:
+        desired_heading_pub (Publisher): Publishes mock desired heading data
+        in `DesiredHeading` message.
+        sail_trim_tab_angle_pub (Publisher): Publishes mock sail trim tab data
+        in `SailCmd` message
+    """
+
+    def __init__(self):
+        super().__init__("mock_data")
+        self.__declare_ros_parameters()
+
+        self.desired_heading_pub = self.create_publisher(
+            msg_type=DesiredHeading,
+            topic=Constants.PHYSICS_ENGINE_SUBSCRIPTIONS.DESIRED_HEADING,
+            qos_profile=self.qos_depth,
+        )
+        self.sail_trim_tab_angle_pub = self.create_publisher(
+            msg_type=SailCmd,
+            topic=Constants.PHYSICS_ENGINE_SUBSCRIPTIONS.SAIL_TRIM_TAB_ANGLE,
+            qos_profile=self.qos_depth,
+        )
+
+        timer_period = self.pub_period  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def __declare_ros_parameters(self):
+        """Declares ROS parameters from the global configuration file that will be used in this
+        node. This node will monitor for any changes to these parameters during execution and will
+        update itself accordingly.
+        """
+        self.get_logger().debug("Declaring ROS parameters...")
+        self.declare_parameters(
+            namespace="",
+            parameters=[
+                ("pub_period_sec", rclpy.Parameter.Type.DOUBLE),
+                ("mock_sail_trim_tab", rclpy.Parameter.Type.BOOL),
+                ("mock_desired_heading", rclpy.Parameter.Type.BOOL),
+                ("qos_depth", rclpy.Parameter.Type.INTEGER),
+                ("mock_desired_heading_lower_bound", rclpy.Parameter.Type.DOUBLE),
+                ("mock_desired_heading_upper_bound", rclpy.Parameter.Type.DOUBLE),
+                ("mock_sail_trim_tab_lower_bound", rclpy.Parameter.Type.DOUBLE),
+                ("mock_sail_trim_tab_upper_bound", rclpy.Parameter.Type.DOUBLE),
+            ],
+        )
+
+        all_parameters = self._parameters
+        for name, parameter in all_parameters.items():
+            value_str = str(parameter.value)
+            self.get_logger().debug(f"Got parameter {name} with value {value_str}")
+
+    def timer_callback(self):
+        if self.mock_desired_heading:
+            self.publish_mock_desired_heading()
+        if self.mock_sail_trim_tab:
+            self.publish_mock_sail_trim_tab_angle()
+
+    def publish_mock_desired_heading(self):
+        """Publishes mock desired heading data."""
+        heading = random.uniform(
+            self.mock_desired_heading_lower_bound, self.mock_desired_heading_upper_bound
+        )
+
+        helper_heading = HelperHeading()
+        helper_heading.heading = heading
+
+        msg = DesiredHeading()
+        msg.heading = helper_heading
+
+        self.desired_heading_pub.publish(msg)
+        self.get_logger().info(
+            f"Publishing to {self.desired_heading_pub.topic} "
+            f"a mock desired heading of {heading} degrees"
+        )
+
+    def publish_mock_sail_trim_tab_angle(self):
+        """Publishes mock sail trim tab angle data."""
+        trim_tab_angle_degrees = random.uniform(
+            self.mock_sail_trim_tab_lower_bound, self.mock_sail_trim_tab_upper_bound
+        )
+
+        msg = SailCmd()
+        msg.trim_tab_angle_degrees = trim_tab_angle_degrees
+
+        self.sail_trim_tab_angle_pub.publish(msg)
+        self.get_logger().info(
+            f"Publishing to {self.sail_trim_tab_angle_pub.topic} "
+            f"a mock trim tab angle of {trim_tab_angle_degrees} degrees"
+        )
+
+    @property
+    def pub_period(self):
+        return self.get_parameter("pub_period_sec").get_parameter_value().double_value
+
+    @property
+    def mock_desired_heading(self):
+        return self.get_parameter("mock_desired_heading").get_parameter_value().bool_value
+
+    @property
+    def mock_sail_trim_tab(self):
+        return self.get_parameter("mock_sail_trim_tab").get_parameter_value().bool_value
+
+    @property
+    def qos_depth(self):
+        return self.get_parameter("qos_depth").get_parameter_value().integer_value
+
+    @property
+    def mock_desired_heading_lower_bound(self):
+        return (
+            self.get_parameter("mock_desired_heading_lower_bound")
+            .get_parameter_value()
+            .double_value
+        )
+
+    @property
+    def mock_desired_heading_upper_bound(self):
+        return (
+            self.get_parameter("mock_desired_heading_upper_bound")
+            .get_parameter_value()
+            .double_value
+        )
+
+    @property
+    def mock_sail_trim_tab_lower_bound(self):
+        return (
+            self.get_parameter("mock_sail_trim_tab_lower_bound").get_parameter_value().double_value
+        )
+
+    @property
+    def mock_sail_trim_tab_upper_bound(self):
+        return (
+            self.get_parameter("mock_sail_trim_tab_upper_bound").get_parameter_value().double_value
+        )
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    try:
+        if is_mock_data_enabled():
+            node = MockDataNode()
+            rclpy.spin(node)
+    finally:
+        rclpy.shutdown()
+
+
+def is_mock_data_enabled() -> bool:
+    try:
+        is_mock_data_enabled_index = sys.argv.index(Constants.MOCK_DATA_CLI_ARG_NAME) + 1
+        is_mock_data_enabled = sys.argv[is_mock_data_enabled_index] == "true"
+    except ValueError:
+        is_mock_data_enabled = False
+    return is_mock_data_enabled
+
+
+if __name__ == "__main__":
+    main()
