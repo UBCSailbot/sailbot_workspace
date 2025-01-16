c361a80345401813097dcd88e31cfbb18ea8fde5
Akshanjay Kompelli <63443017+FireBoyAJ24@users.noreply.github.com>
2025-01-15 17:44:39 -0800

    Created a new integration-test workflow

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# give user permissions, required for GitHub Actions
+ls -la
+sudo chown -R $(whoami):$(whoami) $ROS_WORKSPACE
+
 echo "Integration tests started"
 
 for FILE in $ROS_WORKSPACE/src/integration_tests/testplans/*; do
