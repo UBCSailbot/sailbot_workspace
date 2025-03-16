a81dacc660c3eb6a2af180985d7997f44a0663b0
Sean Donaghy <118148642+SPDonaghy@users.noreply.github.com>
2025-03-15 17:05:45 -0700

    install_ompl.sh works

--- src/local_pathfinding/src/build/install_ompl.sh
+++ src/local_pathfinding/src/build/install_ompl.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-
 set -e
 
-# this will detect the underlying architecture and generate the appropriate bindings (for x86 or aarch64)
-g++ -O3 -Wall -shared -std=c++11 -fPIC `python3 -m pybind11 --includes` /workspaces/sailbot_workspace/src/local_pathfinding/src/build/ompl_bindings.cpp -o /workspaces/sailbot_workspace/src/local_pathfinding/src/build/pyompl`python3-config --extension-suffix` -I/usr/include/ompl-1.6 -I/usr/include/eigen3 -L/usr/share -lompl
-
-# we need to create symbolic links to the python bindings so that they can be imported during tests and when ROS is running
 arch=$(uname -m)
+AARCH_64_WHL_URL="https://github.com/ompl/ompl/releases/download/prerelease/ompl-1.6.0-cp310-cp310-manylinux_2_28_aarch64.whl"
+AARCH_64_WHL_FILE="/tmp/ompl-1.6.0-cp310-cp310-manylinux_2_28_aarch64.whl"
+X86_64_WHL_URL="https://github.com/ompl/ompl/releases/download/prerelease/ompl-1.6.0-cp310-cp310-manylinux_2_28_x86_64.whl"
+X86_64_WHL_FILE="/tmp/ompl-1.6.0-cp310-cp310-manylinux_2_28_x86_64.whl"
 
 if [ "$arch" = "x86_64" ]; then
     echo "running on x86_64 architecture."
-    sudo ln -sf /workspaces/sailbot_workspace/src/local_pathfinding/src/build/pyompl.cpython-310-x86_64-linux-gnu.so /workspaces/sailbot_workspace/install/lib/python3.10/site-packages
-    sudo ln -sf /workspaces/sailbot_workspace/src/local_pathfinding/src/build/pyompl.cpython-310-x86_64-linux-gnu.so /usr/lib/python3/dist-packages
+    curl -L -o "$X86_64_WHL_FILE" "$X86_64_WHL_URL"
+    pip3 install "$X86_64_WHL_FILE"
+    rm -f "$X86_64_WHL_FILE"
+    # sudo ln -sf /path/to/ompl/installation /workspaces/sailbot_workspace/install/lib/python3.10/site-packages
+    # sudo ln -sf /path/to/ompl/installation /usr/lib/python3/dist-packages
 elif [ "$arch" = "aarch64" ]; then
     echo "running on aarch64 architecture."
-    sudo ln -sf /workspaces/sailbot_workspace/src/local_pathfinding/src/build/pyompl.cpython-310-aarch64-linux-gnu.so /workspaces/sailbot_workspace/install/lib/python3.10/site-packages
-    sudo ln -sf /workspaces/sailbot_workspace/src/local_pathfinding/src/build/pyompl.cpython-310-aarch64-linux-gnu.so /usr/lib/python3/dist-packages
+    curl -L -o "$AARCH_64_WHL_FILE" "$AARCH_64_WHL_URL"
+    pip3 install "$AARCH_64_WHL_FILE"
+    rm -f "$AARCH_64_WHL_FILE"
+    # sudo ln -sf /path/to/ompl/installation /workspaces/sailbot_workspace/install/lib/python3.10/site-packages
+    # sudo ln -sf /path/to/ompl/installation /usr/lib/python3/dist-packages
 else
     echo "Unknown architecture: $arch"
 fi
 
-echo "Python bindings for ompl installed successfully"
+if pip3 show ompl 2>&1 | grep -q "not found"; then
+    echo "ompl installation failed"
+fi
