4845f2abb0528f66bc90776c53db04f7c9dfde93
JordanChen123 <jordanthechen@gmail.com>
2025-04-25 22:33:01 -0700

    minor change so redux logs are cleared in prod env as well

--- src/website/lib/redux/logUtils.ts
+++ src/website/lib/redux/logUtils.ts
@@ -1,9 +1,5 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 export const clearLogsPeriodically = () => {
-  if (isDevelopment) {
-    setInterval(() => {
-      console.clear();
-    }, 60000); // Clear logs every minute
-  }
+  setInterval(() => {
+    console.clear();
+  }, 60000); // Clear logs every minute
 };
