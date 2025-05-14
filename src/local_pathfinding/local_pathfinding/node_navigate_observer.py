f79b8e02b7e8d08ce942c70651f48e07db307a61
Akshanjay Kompelli <63443017+FireBoyAJ24@users.noreply.github.com>
2025-05-14 08:50:17 -0700

    Added a temp animated live graph feature

--- src/local_pathfinding/local_pathfinding/node_navigate_observer.py
+++ src/local_pathfinding/local_pathfinding/node_navigate_observer.py
@@ -8,12 +8,10 @@ from typing import List
 
 import custom_interfaces.msg as ci
 import dash
-import plotly
 import plotly.graph_objects as go
 import rclpy
 from dash import dcc, html
 from dash.dependencies import Input, Output
-from plotly.io._base_renderers import BaseHTTPRequestHandler, HTTPServer
 from plotly.validators.scatter.marker import SymbolValidator
 from rclpy.node import Node
 
@@ -128,7 +126,6 @@ class SailbotObserver(Node):
             self.get_logger().info("Message is the same as previous, skipping update.")
             return
 
-
         self.prev_lpath_data = msg
         self.msgs.append(msg)
 
@@ -149,128 +146,18 @@ class SailbotObserver(Node):
 
         return False
 
-    # def final_plot(self, state: VisualizerState):
-    #     if state is None:
-    #         self.get_logger().info("No state to plot.")
-
-    #     # Initial plot
-    #     center_index = len(state.all_waypoints_pos_xy[-1]) // 2
-    #     num_waypoints = len(state.all_waypoints_pos_xy[-1])
-    #     initial_boat_state = go.Scatter(
-    #         x=[state.sailbot_pos_x[0]],
-    #         y=[state.sailbot_pos_y[0]],
-    #         mode="markers",
-    #         marker_symbol=self.raw_symbols,
-    #         marker_line_color="darkseagreen",
-    #         marker_color="lightgreen",
-    #         marker_line_width=2,
-    #         marker_size=15,
-    #         text=["Boat"],
-    #         name="Boat",
-    #     )
-    #     initial_state = [
-    #         go.Scatter(
-    #             x=[state.all_local_waypoints_x[0]],
-    #             y=[state.all_local_waypoints_y[0]],
-    #             mode="markers",
-    #             marker=go.scatter.Marker(size=14),
-    #             text=["Start"],
-    #             name="Start",
-    #         ),
-    #         go.Scatter(
-    #             x=state.all_local_waypoints_x[1 : num_waypoints - 1],
-    #             y=state.all_local_waypoints_y[1 : num_waypoints - 1],
-    #             mode="markers",
-    #             marker=go.scatter.Marker(size=14),
-    #             text=["Intermediate"] * (num_waypoints - 2),
-    #             name="Intermediate",
-    #         ),
-    #         go.Scatter(
-    #             x=[state.all_local_waypoints_x[-1]],
-    #             y=[state.all_local_waypoints_y[-1]],
-    #             mode="markers",
-    #             marker=go.scatter.Marker(size=14),
-    #             text=["Goal"] * (num_waypoints - 2),
-    #             name="Goal",
-    #         ),
-    #     ]
-    #     new_frames = [
-    #         go.Frame(
-    #             data=initial_state
-    #             + [
-    #                 go.Scatter(
-    #                     x=[state.sailbot_pos_x[i]],
-    #                     y=[state.sailbot_pos_y[i]],
-    #                     mode="markers",
-    #                     marker_symbol=self.raw_symbols,
-    #                     marker_line_color="darkseagreen",
-    #                     marker_color="lightgreen",
-    #                     marker_line_width=2,
-    #                     marker_size=15,
-    #                     text=["Boat"],
-    #                     name="Boat",
-    #                 )
-    #             ],
-    #             name=f"Boat {i}",
-    #         )
-    #         for i in range(0, len(state.sailbot_pos_xy))
-    #     ]
-    #     fig = go.Figure(
-    #         data=initial_state + [initial_boat_state],
-    #         layout=go.Layout(
-    #             mapbox_style="open-street-map",
-    #             mapbox_zoom=7,
-    #             mapbox_center={
-    #                 "X": state.all_local_waypoints_x[center_index],
-    #                 "Y": state.all_local_waypoints_x[center_index],
-    #             },
-    #             margin={"r": 0, "t": 0, "l": 0, "b": 0},
-    #             xaxis=dict(title="X"),
-    #             yaxis=dict(title="Y"),
-    #             updatemenus=[
-    #                 dict(
-    #                     type="buttons",
-    #                     showactive=False,
-    #                     buttons=[
-    #                         dict(
-    #                             label="Play",
-    #                             method="animate",
-    #                             args=[
-    #                                 None,
-    #                                 {
-    #                                     "frame": {"duration": 1000, "redraw": True},
-    #                                     "mode": "immediate",
-    #                                 },
-    #                             ],
-    #                         ),
-    #                         dict(
-    #                             label="Pause",
-    #                             method="animate",
-    #                             args=[
-    #                                 [None],
-    #                                 {
-    #                                     "frame": {"duration": 0, "redraw": True},
-    #                                     "mode": "immediate",
-    #                                 },
-    #                             ],
-    #                         ),
-    #                     ],
-    #                 )
-    #             ],
-    #         ),
-    #         frames=new_frames,
-    #     )
-
-    #     fig.show()
-
 
 def dash_app():
 
     app.layout = html.Div(
         [
+            html.H2("Live Path Planning"),
             dcc.Graph(id="live-graph"),
             dcc.Interval(id="interval-component", interval=5000, n_intervals=0),
-        ]
+            html.H2("Animated Path Planning"),
+            dcc.Graph(id="animated-live-graph"),
+            dcc.Interval(id="interval-component2", interval=10000, n_intervals=0),
+        ],
     )
     app.title = "Sailbot Path Planning"
     app.run(debug=True, use_reloader=False)
@@ -292,14 +179,23 @@ def initial_plot():
 
 
 @app.callback(Output("live-graph", "figure"), [Input("interval-component", "n_intervals")])
-def plot(n_intervals):
+def live_plot(n_intervals):
     state = queue.get()
-    fig = update_plot(state)
+    fig = live_update_plot(state)
     return fig
 
 
-def update_plot(state: VisualizerState):
-    raw_symbols = 'arrow'
+@app.callback(
+    Output("animated-live-graph", "figure"), [Input("interval-component2", "n_intervals")]
+)
+def animated_plot(n_intervals):
+    state = queue.get()
+    fig = animated_update_plot(state)
+    return fig
+
+
+def live_update_plot(state: VisualizerState):
+    raw_symbols = "arrow"
     fig = initial_plot()
 
     # Plotting Local Waypoints
@@ -363,6 +259,123 @@ def update_plot(state: VisualizerState):
         yaxis=dict(range=[y_min, y_max]),
         legend=dict(x=0, y=1),  # Position the legend at the top left
         showlegend=True,
+    )
+
+    return fig
+
+
+def animated_update_plot(state: VisualizerState):
+    fig = initial_plot()
+    # Set axis limits dynamically
+    x_min = min(state.last_local_waypoints_x) - 10
+    x_max = max(state.last_local_waypoints_x) + 10
+    y_min = min(state.last_local_waypoints_y) - 10
+    y_max = max(state.last_local_waypoints_y) + 10
+
+    # Initial plot
+    num_waypoints = len(state.all_waypoints_pos_xy[-1])
+    initial_boat_state = go.Scatter(
+        x=[state.sailbot_pos_x[0]],
+        y=[state.sailbot_pos_y[0]],
+        mode="markers",
+        marker_symbol="arrow",
+        marker_line_color="darkseagreen",
+        marker_color="lightgreen",
+        marker_line_width=2,
+        marker_size=15,
+        text=["Boat"],
+        name="Boat",
+    )
+    initial_state = [
+        go.Scatter(
+            x=[state.all_local_waypoints_x[0]],
+            y=[state.all_local_waypoints_y[0]],
+            mode="markers",
+            marker=go.scatter.Marker(size=14),
+            text=["Start"],
+            name="Start",
+        ),
+        go.Scatter(
+            x=state.all_local_waypoints_x[1 : num_waypoints - 1],
+            y=state.all_local_waypoints_y[1 : num_waypoints - 1],
+            mode="markers",
+            marker=go.scatter.Marker(size=14),
+            text=["Intermediate"] * (num_waypoints - 2),
+            name="Intermediate",
+        ),
+        go.Scatter(
+            x=[state.all_local_waypoints_x[-1]],
+            y=[state.all_local_waypoints_y[-1]],
+            mode="markers",
+            marker=go.scatter.Marker(size=14),
+            text=["Goal"] * (num_waypoints - 2),
+            name="Goal",
+        ),
+    ]
+    new_frames = [
+        go.Frame(
+            data=initial_state
+            + [
+                go.Scatter(
+                    x=[state.sailbot_pos_x[i]],
+                    y=[state.sailbot_pos_y[i]],
+                    mode="markers",
+                    marker_symbol="arrow",
+                    marker_line_color="darkseagreen",
+                    marker_color="lightgreen",
+                    marker_line_width=2,
+                    marker_size=15,
+                    text=["Boat"],
+                    name="Boat",
+                )
+            ],
+            name=f"Boat {i}",
+        )
+        for i in range(0, len(state.sailbot_pos_xy))
+    ]
+    fig = go.Figure(
+        data=initial_state + [initial_boat_state],
+        layout=go.Layout(
+            xaxis_title="X Coordinate",
+            yaxis_title="Y Coordinate",
+            xaxis=dict(range=[x_min, x_max]),
+            yaxis=dict(range=[y_min, y_max]),
+            legend=dict(x=0, y=1),  # Position the legend at the top left
+            showlegend=True,
+            updatemenus=[
+                dict(
+                    type="buttons",
+                    showactive=False,
+                    buttons=[
+                        dict(
+                            label="Play",
+                            method="animate",
+                            args=[
+                                None,
+                                {
+                                    "frame": {"duration": 1000, "redraw": True},
+                                    "mode": "immediate",
+                                    "fromcurrent": True,
+                                },
+                            ],
+                        ),
+                        dict(
+                            label="Pause",
+                            method="animate",
+                            args=[
+                                [None],
+                                {
+                                    "frame": {"duration": 0, "redraw": True},
+                                    "mode": "immediate",
+                                    "fromcurrent": False,
+                                },
+                            ],
+                        ),
+                    ],
+                )
+            ],
+        ),
+        frames=new_frames,
     )
 
     return fig
