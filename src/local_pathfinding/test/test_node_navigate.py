26155d8511437b3ea291fb839440f25767698393
Jamen Kaye <71156197+jamenkaye@users.noreply.github.com>
2025-06-01 22:35:05 -0700

    Added target global waypoint tracking

--- src/local_pathfinding/test/test_node_navigate.py
+++ src/local_pathfinding/test/test_node_navigate.py
@@ -3,6 +3,12 @@ import pytest
 
 import local_pathfinding.node_navigate as nn
 
+LOCAL_WAYPOINT_REACHED_THRESH_KM = 0.5
+GLOBAL_WAYPOINT_REACHED_THRESH_KM = 3
+PATHFINDING_RANGE_KM = 30
+ONE_DEGREE_KM = 111  # One degree longitude at equator = 111km
+PATH_RANGE_DEG = PATHFINDING_RANGE_KM / ONE_DEGREE_KM
+
 
 @pytest.mark.parametrize(
     "path, waypoint_index, boat_lat_lon, correct_heading, new_wp_index",
@@ -55,3 +61,57 @@ def test_calculate_desired_heading_and_waypoint_index(
     )
     assert calculated_answer[0] == pytest.approx(correct_heading, abs=3e-1)
     assert calculated_answer[1] == new_wp_index
+
+
+@pytest.mark.parametrize(
+    "global_path, boat_lat_lon, correct_index",
+    [
+        (
+            ci.Path(waypoints=[ci.HelperLatLon(latitude=0.0, longitude=0.0)]),
+            ci.HelperLatLon(latitude=0.0, longitude=-0.1),
+            0,
+        ),
+        (
+            ci.Path(
+                waypoints=[
+                    ci.HelperLatLon(latitude=0.0, longitude=0.0),
+                    ci.HelperLatLon(latitude=0.0, longitude=1 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=0.0, longitude=2 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=0.0, longitude=3 * PATH_RANGE_DEG),
+                ]
+            ),
+            ci.HelperLatLon(latitude=0.98 * PATH_RANGE_DEG, longitude=2 * PATH_RANGE_DEG),
+            2,
+        ),
+        (
+            ci.Path(
+                waypoints=[
+                    ci.HelperLatLon(latitude=0.0, longitude=0.0),
+                    ci.HelperLatLon(latitude=0.0, longitude=1 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=0.0, longitude=2 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=0.0, longitude=3 * PATH_RANGE_DEG),
+                ]
+            ),
+            ci.HelperLatLon(latitude=1.02 * PATH_RANGE_DEG, longitude=1 * PATH_RANGE_DEG),
+            1,
+        ),
+        (
+            ci.Path(
+                waypoints=[
+                    ci.HelperLatLon(latitude=1.0 * PATH_RANGE_DEG, longitude=1 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=1.0 * PATH_RANGE_DEG, longitude=2 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=0.0, longitude=2 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=0.0, longitude=1 * PATH_RANGE_DEG),
+                    ci.HelperLatLon(latitude=0.0, longitude=0.0),
+                ]
+            ),
+            ci.HelperLatLon(latitude=0.98 * PATH_RANGE_DEG, longitude=1 * PATH_RANGE_DEG),
+            1,
+        ),
+    ],
+)
+def test_find_next_global_waypoint_index(
+    global_path: ci.Path, boat_lat_lon: ci.HelperLatLon, correct_index: int
+):
+    calculated_answer = nn.Sailbot.find_next_global_waypoint_index(global_path, boat_lat_lon)
+    assert calculated_answer == correct_index
