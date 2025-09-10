9a3840d8d328e1278934661965c625ce0b307d02
fyang151 <fyang151@gmail.com>
2025-09-10 14:44:22 -0700

    Update title.

--- src/website/views/components/Header/Header.tsx
+++ src/website/views/components/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className={styles.header}>
       <div className={styles.title}>
         <img src='LogoPlainWhite.svg' alt='Logo' className={styles.logo} />
-        <h1>POLARISTRACKER.COM</h1>
+        <h1>SAILBOTPOLARIS.COM</h1>
       </div>
       <div className={styles.links}>
         <Link href='/'>DASHBOARD</Link>
