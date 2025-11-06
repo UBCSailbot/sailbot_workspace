0021d760be58dee1d7cfee856892c89e75d7c6c8
AMaharaj16 <aayush.maharaj16@gmail.com>
2025-11-05 20:26:43 -0800

    Moved test_calculate_desired_heading_and_waypoint_index from test_node_navigate to test_local_path.

--- src/local_pathfinding/test/test_local_path.py
+++ src/local_pathfinding/test/test_local_path.py
@@ -11,11 +11,64 @@ from shapely.geometry import MultiPolygon, Polygon
 
 import local_pathfinding.coord_systems as cs
 import local_pathfinding.local_path as lp
+import custom_interfaces.msg as ci
 from local_pathfinding.obstacles import Obstacle
 
 REF = HelperLatLon(latitude=10.0, longitude=10.0)
 
 PATH = lp.LocalPath(parent_logger=RcutilsLogger())
+
+@pytest.mark.parametrize(
+    "path, waypoint_index, boat_lat_lon, correct_heading, new_wp_index",
+    [
+        (
+            ci.Path(waypoints=[ci.HelperLatLon(latitude=0.0, longitude=0.0)]),
+            0,
+            ci.HelperLatLon(latitude=0.0, longitude=-0.1),
+            90.0,
+            0,
+        ),
+        (
+            ci.Path(waypoints=[ci.HelperLatLon(latitude=0.0, longitude=0.0)]),
+            0,
+            ci.HelperLatLon(latitude=0.1, longitude=0.0),
+            -180.0,
+            0,
+        ),
+        (
+            ci.Path(waypoints=[ci.HelperLatLon(latitude=0.0, longitude=0.0)]),
+            0,
+            ci.HelperLatLon(latitude=0.1, longitude=0.1),
+            -135.0,
+            0,
+        ),
+        (
+            # Test: boat has reached waypoints[0], heading should be to waypoints[1].
+            ci.Path(
+                waypoints=[
+                    ci.HelperLatLon(latitude=0.0, longitude=0.1),
+                    ci.HelperLatLon(latitude=0.0, longitude=0.0),
+                ]
+            ),
+            0,
+            ci.HelperLatLon(latitude=0.0, longitude=0.09999),
+            -90.0,
+            1,
+        ),
+    ],
+)
+def test_calculate_desired_heading_and_waypoint_index(
+    path: ci.Path,
+    waypoint_index: int,
+    boat_lat_lon: ci.HelperLatLon,
+    correct_heading: float,
+    new_wp_index: int,
+):
+    calculated_answer = PATH.calculate_desired_heading_and_waypoint_index(
+        path, waypoint_index, boat_lat_lon
+    )
+    assert calculated_answer[0] == pytest.approx(correct_heading, abs=3e-1)
+    assert calculated_answer[1] == new_wp_index
 
 
 @pytest.mark.parametrize(
--- src/local_pathfinding/test/test_node_navigate.py
+++ src/local_pathfinding/test/test_node_navigate.py
@@ -11,59 +11,6 @@ PATH_RANGE_DEG = PATHFINDING_RANGE_KM / ONE_DEGREE_KM
 
 
 @pytest.mark.parametrize(
-    "path, waypoint_index, boat_lat_lon, correct_heading, new_wp_index",
-    [
-        (
-            ci.Path(waypoints=[ci.HelperLatLon(latitude=0.0, longitude=0.0)]),
-            0,
-            ci.HelperLatLon(latitude=0.0, longitude=-0.1),
-            90.0,
-            0,
-        ),
-        (
-            ci.Path(waypoints=[ci.HelperLatLon(latitude=0.0, longitude=0.0)]),
-            0,
-            ci.HelperLatLon(latitude=0.1, longitude=0.0),
-            -180.0,
-            0,
-        ),
-        (
-            ci.Path(waypoints=[ci.HelperLatLon(latitude=0.0, longitude=0.0)]),
-            0,
-            ci.HelperLatLon(latitude=0.1, longitude=0.1),
-            -135.0,
-            0,
-        ),
-        (
-            # Test: boat has reached waypoints[0], heading should be to waypoints[1].
-            ci.Path(
-                waypoints=[
-                    ci.HelperLatLon(latitude=0.0, longitude=0.1),
-                    ci.HelperLatLon(latitude=0.0, longitude=0.0),
-                ]
-            ),
-            0,
-            ci.HelperLatLon(latitude=0.0, longitude=0.09999),
-            -90.0,
-            1,
-        ),
-    ],
-)
-def test_calculate_desired_heading_and_waypoint_index(
-    path: ci.Path,
-    waypoint_index: int,
-    boat_lat_lon: ci.HelperLatLon,
-    correct_heading: float,
-    new_wp_index: int,
-):
-    calculated_answer = LocalPath.calculate_desired_heading_and_waypoint_index(
-        path, waypoint_index, boat_lat_lon
-    )
-    assert calculated_answer[0] == pytest.approx(correct_heading, abs=3e-1)
-    assert calculated_answer[1] == new_wp_index
-
-
-@pytest.mark.parametrize(
     "global_path, boat_lat_lon, correct_index",
     [
         (
