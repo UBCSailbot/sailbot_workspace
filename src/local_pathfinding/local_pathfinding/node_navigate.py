960145a2eea71fd2e7a1601b4bee69ab71e578e9
Raghumani Mehta <raghumani2003@outlook.com>
2026-02-18 15:52:52 -0800

    comment changes

--- src/local_pathfinding/local_pathfinding/node_navigate.py
+++ src/local_pathfinding/local_pathfinding/node_navigate.py
@@ -52,9 +52,8 @@ class Sailbot(Node):
         global_path (ci.Path): Path that we are following.
         filtered_wind_sensor (ci.WindSensor): Filtered data from the wind sensors.
         desired_heading (ci.DesiredHeading): current desired heading.
-        prev_lp_wp_index: index of the last local waypoint seen. This is the waypoint that has
-        been traversed by the boat. Polaris is now moving towards prev_lp_wp_index + 1 index of the
-        local path.
+        prev_lp_wp_index: local waypoint that Polaris has already traversed/seen. Polaris is moving
+        towards prev_lp_wp_index + 1 waypoint.
 
     Attributes:
         local_path (LocalPath): The path that `Sailbot` is following.
