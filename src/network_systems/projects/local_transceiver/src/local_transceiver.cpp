adbb83dc7a3aa109a014bdb7fc83af9fbdee11d4
lross03 <136214176+lross03@users.noreply.github.com>
2026-02-28 16:37:34 -0800

    cleanup unnecessary comment

--- src/network_systems/projects/local_transceiver/src/local_transceiver.cpp
+++ src/network_systems/projects/local_transceiver/src/local_transceiver.cpp
@@ -565,7 +565,6 @@ custom_interfaces::msg::Path LocalTransceiver::receive()
 
     std::future<void> fut = std::async(std::launch::async, cacheGlobalWaypoints, receivedDataBuffer);
 
-    // For debugging: print the hex representation of the received data buffer
     custom_interfaces::msg::Path to_publish = parseInMsg(receivedDataBuffer);
 
     fut.get();
