e01c6afc4a19d51e4558a072770dec593b693f14
Jordan Chen <jordanthechen@gmail.com>
2026-03-03 16:33:21 -0800

    fix prod api calls - uses relative URLS instead of absolute IPs

--- src/website/stores/LocalPath/LocalPathService.ts
+++ src/website/stores/LocalPath/LocalPathService.ts
@@ -1,3 +1,5 @@
+import { getApiUrl } from '@/lib/apiUrl';
+
 /**
  * Defines all saga methods to make requests to the LocalPath interface.
  */
@@ -6,7 +8,7 @@ export const LocalPathService = {
     let isError = false;
 
     return yield fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_HOST}:${process.env.NEXT_PUBLIC_SERVER_PORT}/api/localpath`,
+      getApiUrl('/api/localpath'),
       {
         method: 'GET',
       },
