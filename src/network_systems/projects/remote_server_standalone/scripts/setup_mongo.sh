0cf2bbe9f61ce6ab595e530008c7c9fba67bfd8c
Aprameya Aithal <67941617+Apps247@users.noreply.github.com>
2026-03-28 16:17:34 -0700

    Commented out apt install

--- src/network_systems/projects/remote_server_standalone/scripts/setup_mongo.sh
+++ src/network_systems/projects/remote_server_standalone/scripts/setup_mongo.sh
@@ -16,3 +16,7 @@ cmake ..                                \
 
 cmake --build .
 sudo cmake --build . --target install
+
+# Install MongoDB C++ driver dependencies
+# sudo apt-get update
+#sudo apt-get install -y libmongocxx-dev libbsoncxx-dev
