2c99e12b09c57b07641900706c0fca315ab639b8
AMaharaj16 <aayush.maharaj16@gmail.com>
2026-04-03 12:30:05 -0700

    Adjusted naming conventions in wind_coord_systems and ompl_objectives: - reduced True Wind/Apparent Wind to tw/aw - added gc and bc to variables/constant/fields accordingly

--- src/local_pathfinding/local_pathfinding/ompl_objectives.py
+++ src/local_pathfinding/local_pathfinding/ompl_objectives.py
@@ -54,8 +54,8 @@ BOAT_SPEEDS = np.array(
     ]
 )
 
-TRUE_WIND_SPEEDS = [0.0, 11.1, 14.8, 18.5, 22.2, 25.9, 29.6, 37.0, 55.0, 75.0]
-SAILING_ANGLES = [0, 45, 50, 60, 75, 90, 110, 120, 135, 150, 180]
+TW_SPEEDS_GC = [0.0, 11.1, 14.8, 18.5, 22.2, 25.9, 29.6, 37.0, 55.0, 75.0]
+SAILING_ANGLES_DEG = [0, 45, 50, 60, 75, 90, 110, 120, 135, 150, 180]
 
 ESTIMATED_TOP_BOAT_SPEED = np.max(BOAT_SPEEDS)
 
@@ -65,16 +65,16 @@ class WindObjective(ob.OptimizationObjective):
     (or almost directly) upwind or downwind.
 
     Attributes:
-        true_wind_direction (float): The direction of the true wind in radians (-pi, pi]
+        wind_direction_rad_gc (float): Direction of wind in global coordinate radians (-pi, pi]
     """
 
     def __init__(
         self,
         space_information,
-        true_wind_direction_radians: float,
+        tw_direction_rad_gc: float,
     ):
         super().__init__(space_information)
-        self.true_wind_direction_radians = true_wind_direction_radians
+        self.tw_direction_rad_gc = tw_direction_rad_gc
 
     def motionCost(self, s1: ob.SE2StateSpace, s2: ob.SE2StateSpace) -> ob.Cost:
         """Defines the cost of a path segment, from s1 to s2, with regards to the direction of the
@@ -91,11 +91,11 @@ class WindObjective(ob.OptimizationObjective):
         s1_xy = cs.XY(s1.getX(), s1.getY())
         s2_xy = cs.XY(s2.getX(), s2.getY())
         return ob.Cost(
-            WindObjective.wind_direction_cost(s1_xy, s2_xy, self.true_wind_direction_radians)
+            WindObjective.wind_direction_cost(s1_xy, s2_xy, self.tw_direction_rad_gc)
         )
 
     @staticmethod
-    def wind_direction_cost(s1: cs.XY, s2: cs.XY, tw_direction_rad: float) -> float:
+    def wind_direction_cost(s1: cs.XY, s2: cs.XY, tw_direction_rad_gc: float) -> float:
         """Computes a wind alignment cost based on the absolute angle θ between the segment
         bearing and the true wind direction.
 
@@ -108,19 +108,19 @@ class WindObjective(ob.OptimizationObjective):
         Args:
             s1 (cs.XY): The start point of the path segment
             s2 (cs.XY): The end point of the path segment
-            tw_direction_rad (float): The direction of the true wind in radians, (-pi, pi]
+            tw_direction_rad_gc (float): The direction of the true wind in radians, (-pi, pi]
 
         Returns:
             float: The cost the path segment from s1 to s2, in the interval [0, 1]
         """
         segment_true_bearing_rad = cs.get_path_segment_true_bearing(s1, s2, rad=True)
-        tw_angle_rad = abs(wcs.get_true_wind_angle(segment_true_bearing_rad, tw_direction_rad))
+        tw_angle_rad_gc = abs(wcs.get_true_wind_angle(segment_true_bearing_rad,
+                                                      tw_direction_rad_gc))
 
-        # NO-GO ZONE
-        if tw_angle_rad <= NO_GO_ZONE or tw_angle_rad >= math.pi - NO_GO_ZONE:
+        if tw_angle_rad_gc <= NO_GO_ZONE or tw_angle_rad_gc >= math.pi - NO_GO_ZONE:
             return 1.0
 
-        cost = math.sin(2*tw_angle_rad) ** WIND_COST_SIN_EXPONENT
+        cost = math.sin(2*tw_angle_rad_gc) ** WIND_COST_SIN_EXPONENT
         return cost
 
 
@@ -130,12 +130,12 @@ class TimeObjective(ob.OptimizationObjective):
     end of the segment.
 
     Attributes:
-        wind_direction (float): The direction of the wind in radians (-pi, pi]
-        wind_speed (float): The speed of the wind in m/s
+        tw_direction_rad_gc (float): The direction of wind in global coordinate radians (-pi, pi]
+        tw_speed_kmph_gc (float): The speed of the true wind in km/h
     """
 
     interpolation = RegularGridInterpolator(
-        (TRUE_WIND_SPEEDS, SAILING_ANGLES),
+        (TW_SPEEDS_GC, SAILING_ANGLES_DEG),
         BOAT_SPEEDS,
         bounds_error=False,  # no error on out of bounds call
         # returns max speed for any input outside the range of the table
@@ -145,12 +145,12 @@ class TimeObjective(ob.OptimizationObjective):
     def __init__(
         self,
         space_information,
-        true_wind_direction_radians: float,
-        true_wind_speed_kmph: float,
+        tw_direction_rad_gc: float,
+        tw_speed_kmph_gc: float,
     ):
         super().__init__(space_information)
-        self.true_wind_direction_radians = true_wind_direction_radians
-        self.true_wind_speed_kmph = true_wind_speed_kmph
+        self.tw_direction_rad_gc = tw_direction_rad_gc
+        self.tw_speed_kmph_gc = tw_speed_kmph_gc
 
     def motionCost(self, s1: ob.SE2StateSpace, s2: ob.SE2StateSpace) -> ob.Cost:
         """Defines the cost of a path segment, from s1 to s2, as the estimated time it will take
@@ -170,24 +170,21 @@ class TimeObjective(ob.OptimizationObjective):
             TimeObjective.time_cost(
                 s1_xy,
                 s2_xy,
-                self.true_wind_direction_radians,
-                self.true_wind_speed_kmph,
+                self.tw_direction_rad_gc,
+                self.tw_speed_kmph_gc,
             )
         )
 
     @staticmethod
-    def time_cost(
-        s1: cs.XY, s2: cs.XY, true_wind_direction_radians: float, true_wind_speed_kmph
-    ) -> float:
+    def time_cost(s1: cs.XY, s2: cs.XY, tw_direction_rad_gc: float, tw_speed_kmph_gc) -> float:
         """Returns a cost proportional to the estimated amount of time it will take for the boat
            to travel from s1 to s2.
 
         Args:
             s1 (cs.XY): The start point of the path segment
             s2 (cs.XY): The end point of the path segment
-            true_wind_direction_radians (float): The direction of the true wind in
-            radians (-pi, pi]
-            true_wind_speed_kmph (float): The true wind speed in km/h
+            tw_direction_rad_gc (float): The direction of wind in global coord radians (-pi, pi]
+            tw_speed_kmph_gc (float): The true wind speed in km/h
 
         Returns:
             float: The cost the path segment from s1 to s2, in the interval [0, 1]
@@ -197,8 +194,8 @@ class TimeObjective(ob.OptimizationObjective):
 
         sailbot_speed = TimeObjective.get_sailbot_speed(
             path_segment_true_bearing_radians,
-            true_wind_direction_radians,
-            true_wind_speed_kmph,
+            tw_direction_rad_gc,
+            tw_speed_kmph_gc,
         )
 
         # exit early to avoid dividing by sailbot_speed when it's close to 0
@@ -216,12 +213,12 @@ class TimeObjective(ob.OptimizationObjective):
     @staticmethod
     def get_sailbot_speed(
         path_segment_true_bearing_rad: float,
-        tw_direction_rad: float,
-        tw_speed_kmph: float,
+        tw_direction_rad_gc: float,
+        tw_speed_kmph_gc: float,
     ) -> float:
 
-        tw_angle_rad = abs(
-            wcs.get_true_wind_angle(path_segment_true_bearing_rad, tw_direction_rad)
+        tw_angle_rad_gc = abs(
+            wcs.get_true_wind_angle(path_segment_true_bearing_rad, tw_direction_rad_gc)
         )
 
         # this bounds the twa to a range of 0 to 180 degrees
@@ -229,33 +226,33 @@ class TimeObjective(ob.OptimizationObjective):
         # on the port or starboard side when it comes to calculating the estimated speed
         # and having the twa in the range of [0, 180] means we don't have to cover negative
         # twa values in the BOAT_SPEEDS table
-        tw_angle_deg = abs(cs.bound_to_180(math.degrees(tw_angle_rad)))
+        tw_angle_deg_gc = abs(cs.bound_to_180(math.degrees(tw_angle_rad_gc)))
 
         # since the twa is bounded to [0, 180], the only time the interpolator would need to
         # use the fill_value is if the tw_speed_kmph is greater than the max accounted for
         # in the BOAT_SPEEDS table, in which case the interpolator will return it's configured
         # fill_value (see interpolation definition at top of class)
-        return TimeObjective.interpolation((tw_speed_kmph, tw_angle_deg))
+        return TimeObjective.interpolation((tw_speed_kmph_gc, tw_angle_deg_gc))
 
 
 def get_sailing_objective(
     space_information,
     simple_setup,
-    boat_heading_degrees: float,
-    boat_speed_kmph: float,
-    apparent_wind_direction_degrees: float,
-    apparent_wind_speed_kmph: float,
+    boat_heading_deg_gc: float,
+    boat_speed_kmph_gc: float,
+    aw_direction_deg_bc: float,
+    aw_speed_kmph_bc: float,
 ) -> ob.OptimizationObjective:
 
     apparent_wind_direction_degrees_global_coordinates = wcs.boat_to_global_coordinate(
-        boat_heading_degrees, apparent_wind_direction_degrees
+        boat_heading_deg_gc, aw_direction_deg_bc
     )
 
     tw_dir_rad, tw_speed_kmph = wcs.get_true_wind(
         apparent_wind_direction_degrees_global_coordinates,
-        apparent_wind_speed_kmph,
-        boat_heading_degrees,
-        boat_speed_kmph,
+        aw_speed_kmph_bc,
+        boat_heading_deg_gc,
+        boat_speed_kmph_gc,
     )
 
     multiObjective = ob.MultiOptimizationObjective(si=space_information)
--- src/local_pathfinding/local_pathfinding/wind_coord_systems.py
+++ src/local_pathfinding/local_pathfinding/wind_coord_systems.py
@@ -25,50 +25,55 @@ class Wind:
     dir_deg: float
 
 
-def boat_to_global_coordinate(boat_heading: float, wind_direction: float):
+def boat_to_global_coordinate(boat_heading_deg_gc: float, aw_direction_deg_bc: float):
     """
     Convert a boat-frame wind direction to a global-frame bearing (degrees) in (-180, 180]
     Args:
-        boat_heading (float): Boat heading in degrees (global frame) in (-180, 180].
-        wind_direction (float): Wind direction in boat coordinates (-180, 180] (degrees).
+        boat_heading_deg_gc (float): Boat heading in degrees (global frame) in (-180, 180].
+        aw_direction_deg_bc (float): Wind direction in boat coordinate degrees (-180, 180].
     Returns:
-        float: Global wind bearing in degrees within (-180, 180].
+        float: Global wind bearing in global coordinate degrees within (-180, 180].
     Examples:
         >>> boat_to_global_coordinate(0, 0)      # 0 + 0 + 180 -> -180
         180
         >>> boat_to_global_coordinate(170, 30)   # 170 + 30 + 180 = 380 -> 20
         20
     """
-    return cs.bound_to_180(boat_heading + wind_direction + 180.0)
+    return cs.bound_to_180(boat_heading_deg_gc + aw_direction_deg_bc + 180.0)
 
 
-def global_to_boat_coordinate(boat_heading: float, global_wind_direction: float):
+def global_to_boat_coordinate(boat_heading_deg_gc: float, tw_direction_deg_gc: float):
     """
     Convert a global-frame wind bearing to a boat-frame wind direction.
     Args:
-        boat_heading (float): Boat heading in degrees (global frame), in (-180, 180].
-        global_wind_direction (float): Wind bearing in global frame (degrees), in (-180, 180].
+        boat_heading_deg_gc (float): Boat heading in degrees (global frame), in (-180, 180].
+        tw_direction_deg_gc (float): Wind direction in global coordinate degrees (-180, 180].
     Returns:
         float: Wind direction in boat coordinates (degrees) within (-180, 180].
+    Examples:
+        >>> global_to_boat_coordinate(0, 0)      # 0 - 0 + 180 -> -180
+        180
+        >>> global_to_boat_coordinate(170, 30)   # 170 - 30 + 180 = 320 -> -40
+        -40
     """
-    return cs.bound_to_180(global_wind_direction - boat_heading + 180.0)
+    return cs.bound_to_180(tw_direction_deg_gc - boat_heading_deg_gc + 180.0)
 
 
 def get_true_wind(
-    aw_dir_deg: float,
-    aw_speed_kmph: float,
-    boat_heading_deg: float,
-    boat_speed_kmph: float,
+    aw_dir_deg_bc: float,
+    aw_speed_kmph_bc: float,
+    boat_heading_deg_gc: float,
+    boat_speed_kmph_gc: float,
     ret_rad: bool = True,
 ) -> tuple[float, float]:
     """Compute the true wind vector from apparent wind and boat motion.
 
     Args:
-        aw_dir_deg (float): Apparent wind direction in degrees (-180, 180]. This is the wind
+        aw_dir_deg_bc (float): Apparent wind direction in degrees (-180, 180]. This is the wind
             as measured relative to the boat (sensor reading).
-        aw_speed_kmph (float): Apparent wind speed in km/h.
-        boat_heading_deg (float): Boat heading in degrees (-180, 180].
-        boat_speed_kmph (float): Boat speed over ground in km/h.
+        aw_speed_kmph_bc (float): Apparent wind speed in km/h.
+        boat_heading_deg_gc (float): Boat heading in degrees (-180, 180].
+        boat_speed_kmph_gc (float): Boat speed over ground in km/h.
         ret_rad (bool): If True, return wind direction in radians. If False, return in degrees.
         NOTE: All the angles are with respect to global true bearing. It is the
         responsibility of the caller to ensure this. Particularly, the apparent wind read by the
@@ -86,18 +91,18 @@ def get_true_wind(
         The function computes vector components in an east/north frame, subtracting the boat
         motion from the apparent wind to obtain the true wind.
     """
-    aw_dir_rad = math.radians(aw_dir_deg)
+    aw_dir_rad_bc = math.radians(aw_dir_deg_bc)
 
     # boat wind is in the direction of the boat heading (reverse of boat heading)
-    bw_dir_rad = math.radians(cs.bound_to_180(boat_heading_deg + 180))
-    aw_east_kmph = aw_speed_kmph * math.sin(aw_dir_rad)
-    aw_north_kmph = aw_speed_kmph * math.cos(aw_dir_rad)
+    bw_dir_rad = math.radians(cs.bound_to_180(boat_heading_deg_gc + 180))
+    aw_east_kmph_bc = aw_speed_kmph_bc * math.sin(aw_dir_rad_bc)
+    aw_north_kmph_bc = aw_speed_kmph_bc * math.cos(aw_dir_rad_bc)
 
-    bw_east_kmph = boat_speed_kmph * math.sin(bw_dir_rad)
-    bw_north_kmph = boat_speed_kmph * math.cos(bw_dir_rad)
+    bw_east_kmph = boat_speed_kmph_gc * math.sin(bw_dir_rad)
+    bw_north_kmph = boat_speed_kmph_gc * math.cos(bw_dir_rad)
 
-    tw_east_kmph = aw_east_kmph - bw_east_kmph
-    tw_north_kmph = aw_north_kmph - bw_north_kmph
+    tw_east_kmph = aw_east_kmph_bc - bw_east_kmph
+    tw_north_kmph = aw_north_kmph_bc - bw_north_kmph
 
     tw_speed_kmph = math.hypot(tw_east_kmph, tw_north_kmph)
     tw_dir_rad = math.atan2(tw_east_kmph, tw_north_kmph)
@@ -113,19 +118,19 @@ def get_true_wind(
 
 
 def get_apparent_wind(
-    tw_dir_deg: float,
-    tw_speed_kmph: float,
-    boat_heading_deg: float,
-    boat_speed_kmph: float,
+    tw_dir_deg_gc: float,
+    tw_speed_kmph_gc: float,
+    boat_heading_deg_gc: float,
+    boat_speed_kmph_gc: float,
     ret_rad: bool = True,
 ) -> tuple[float, float]:
     """Compute the apparent wind vector from true wind and boat motion.
 
     Args:
-        tw_dir_deg (float): True wind direction in degrees (-180, 180].
-        tw_speed_kmph (float): True wind speed in km/h.
-        boat_heading_deg (float): Boat heading in degrees (-180, 180].
-        boat_speed_kmph (float): Boat speed over ground in km/h.
+        tw_dir_deg_gc (float): True wind direction in degrees (-180, 180].
+        tw_speed_kmph_gc (float): True wind speed in km/h.
+        boat_heading_deg_gc (float): Boat heading in degrees (-180, 180].
+        boat_speed_kmph_gc (float): Boat speed over ground in km/h.
         ret_rad (bool): If True, return wind direction in radians. If False, return in degrees.
         NOTE: All the angles are with respect to global true bearing. It is the
         responsibility of the caller to ensure this.
@@ -138,20 +143,20 @@ def get_apparent_wind(
         returns (0.0, 0.0). NOTE: The caller is responsible for handling this case, otherwise the
         calculations will break down.
     """
-    tw_dir_rad = math.radians(tw_dir_deg)
+    tw_dir_rad = math.radians(tw_dir_deg_gc)
 
-    bw_speed_kmph = boat_speed_kmph
-    bw_dir_rad = math.radians(cs.bound_to_180(boat_heading_deg + 180.0))
+    bw_speed_kmph = boat_speed_kmph_gc
+    bw_dir_rad = math.radians(cs.bound_to_180(boat_heading_deg_gc + 180.0))
     bw_east_kmph = bw_speed_kmph * math.sin(bw_dir_rad)
     bw_north_kmph = bw_speed_kmph * math.cos(bw_dir_rad)
 
-    tw_east_kmph = tw_speed_kmph * math.sin(tw_dir_rad)
-    tw_north_kmph = tw_speed_kmph * math.cos(tw_dir_rad)
+    tw_east_kmph = tw_speed_kmph_gc * math.sin(tw_dir_rad)
+    tw_north_kmph = tw_speed_kmph_gc * math.cos(tw_dir_rad)
 
     aw_east_kmph = tw_east_kmph + bw_east_kmph
     aw_north_kmph = tw_north_kmph + bw_north_kmph
 
-    aw_speed_kmph = math.hypot(aw_east_kmph, aw_north_kmph)
+    aw_speed_kmph_gc = math.hypot(aw_east_kmph, aw_north_kmph)
     aw_dir_rad = math.atan2(aw_east_kmph, aw_north_kmph)
 
     if ret_rad:
@@ -159,12 +164,12 @@ def get_apparent_wind(
     else:
         aw_dir = math.degrees(aw_dir_rad)
 
-    if aw_speed_kmph > FLOATING_POINT_ERROR_THRESHOLD:
-        return aw_dir, aw_speed_kmph
+    if aw_speed_kmph_gc > FLOATING_POINT_ERROR_THRESHOLD:
+        return aw_dir, aw_speed_kmph_gc
     return ZERO_VECTOR_CONSTANT, 0.0
 
 
-def get_true_wind_angle(boat_heading_rad: float, tw_dir_rad: float) -> float:
+def get_true_wind_angle(boat_heading_rad_gc: float, tw_dir_rad_gc: float) -> float:
     """Calculates the true wind angle (TWA) in radians.
     The TWA is the angle between the heading of a vessel and the true wind direction.
 
@@ -178,9 +183,9 @@ def get_true_wind_angle(boat_heading_rad: float, tw_dir_rad: float) -> float:
 
     Args:
         boat_heading_rad (float): _description_
-        tw_dir_rad (float): _description_
+        tw_dir_rad_gc (float): _description_
 
     Returns:
-        float: The true wind angle in the range (-180, 180]
+        float: The true wind angle in the range (-pi, pi]
     """
-    return cs.bound_to_180((tw_dir_rad - boat_heading_rad), rad=True)
+    return cs.bound_to_180((tw_dir_rad_gc - boat_heading_rad_gc), rad=True)
