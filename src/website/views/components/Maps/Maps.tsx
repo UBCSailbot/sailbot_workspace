0cbab66bbdff38f0a2a275e79c73a1892a3e46e5
Andy Guo <zeyuguo2006@gmail.com>
2026-09-07 17:33:22 -0700

    fix: added carto api key

--- src/website/views/components/Maps/Maps.tsx
+++ src/website/views/components/Maps/Maps.tsx
@@ -225,7 +225,7 @@ const Maps: React.FC<IMapsProps> = ({
       /> */}
       <TileLayer
         attribution='&copy; OpenStreetMap contributors &copy; CARTO'
-        url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+        url={`https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=${process.env.NEXT_PUBLIC_CARTO_API_KEY ?? ''}`}
       />
 
       {showAIShips && <LayerGroup>{renderShips()}</LayerGroup>}
